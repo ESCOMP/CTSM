1dc79a084ab3fa42a972ba425ef2b17101da59d6
Erik Kluzek <erik@ucar.edu>
2010-01-06 07:08:48 +0000

    clm3_6_60
    Fix clm template

--- test/system/test_driver.sh
+++ test/system/test_driver.sh
@@ -20,7 +20,7 @@
 
 #will attach timestamp onto end of script name to prevent overwriting
 cur_time=`date '+%H:%M:%S'`
-seqccsm_vers="ccsm4_0_beta37"
+seqccsm_vers="ccsm4_0_beta38"
 
 hostname=`hostname`
 case $hostname in
