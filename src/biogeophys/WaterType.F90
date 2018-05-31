59646a8b3fe4281e10d7ba5097fcdf1a38ae7e16
Bill Sacks <sacks@ucar.edu>
2018-05-31 07:03:12 -0600

    Remove the water_bulk_type and water_tracer_type containers
    This is design (D5) in the pdf in PR #395, which is the favorite of
    Mariana Vertenstein and Mat Rothstein.
    
    In discussion with Mariana and Mat, we felt that the benefits of having
    these containers weren't worth the extra complexity of having this extra
    layer. Part of my original motivation for having this extra layer was
    thinking that we'd eventually have a lot more objects contained in each
    tracer instance: all of the process-oriented objects like irrigation,
    infiltration excess runoff, etc. (which each compute one or more water
    fluxes). But as I noted in
    https://github.com/ESCOMP/ctsm/pull/395#issuecomment-392299340, I'm now
    feeling that won't be feasible, so we'll probably have no more than 5 or
    6 objects for each tracer instance (state, diagnostics, water balance,
    fluxes, and maybe we'll split one or two of those further). So there is
    less of a need for this tracer type container.

--- src/biogeophys/WaterType.F90
+++ src/biogeophys/WaterType.F90
@@ -1,16 +1,20 @@
 module WaterType
 
-  ! This type encapsulates the management of multiple tracer instances, preventing that
-  ! logic from making its way all the way up to clm_inst.
-  !
   ! This is the only type that will appear directly in clm_instMod: clm_inst will have a
   ! single instance of water_type, named water_inst, and will call water_inst%Init and
   ! water_inst%Restart.
   type, public :: water_type
-     type(waterbulk_type) :: waterbulk_inst
+     type(waterstate_bulk_type) :: waterstate_bulk_inst
+     type(waterdiag_bulk_type) :: waterdiag_bulk_inst
+     type(waterbalance_type) :: waterbalance_bulk_inst
 
-     ! Mat: You don't need to implement this now: This will come in the next iteration
-     type(watertracer_type), allocatable :: watertracer_inst(:)
+     ! Probably move fluxes in here, too - eventually
+
+     ! Mat: You don't need to implement these tracer instances now: These will come in
+     ! the next iteration
+     type(waterstate_type), allocatable :: waterstate_tracer_inst(:)
+     type(waterdiag_type), allocatable :: waterdiag_tracer_inst(:)
+     type(waterbalance_type), allocatable :: waterbalance_tracer_inst(:)
    contains
      procedure :: Init
      procedure :: Restart
@@ -19,24 +23,34 @@ module WaterType
 contains
 
   subroutine Init(this, bounds, ...)
-    call this%waterbulk_inst%Init(bounds, ...)
+    call this%waterstate_bulk_inst%Init(bounds, ...)
+    call this%waterdiag_bulk_inst%Init(bounds, ...)
+    call this%waterbalance_bulk_inst%Init(bounds, ...)
 
     ! Mat: You don't need to implement the following now: This will come in the next iteration
 
-    ! Determine number of tracers
+    ! First have code to determine number of tracers, then do the following:
 
-    allocate(this%watertracer_inst(ntracers))
+    allocate(this%waterstate_tracer_inst(ntracers))
+    allocate(this%waterdiag_tracer_inst(ntracers))
+    allocate(this%waterbalance_tracer_inst(ntracers))
     do i = 1, size(this%watertracer_inst)
-       call this%watertracer_inst(i)%Init(bounds, ...)
+       call this%waterstate_tracer_inst(i)%Init(bounds, ...)
+       call this%waterdiag_tracer_inst(i)%Init(bounds, ...)
+       call this%waterbalance_tracer_inst(i)%Init(bounds, ...)
     end do
   end subroutine Init
 
   subroutine Restart(this, bounds, ...)
-    call this%waterbulk_inst%Restart(bounds, ...)
+    call this%waterstate_inst%Restart(bounds, ...)
+    call this%waterdiag_inst%Restart(bounds, ...)
+    call this%waterbalance_inst%Restart(bounds, ...)
 
     ! Mat: You don't need to implement the following now: This will come in the next iteration
     do i = 1, size(watertracer_inst)
-       call this%watertracer_inst(i)%Restart(bounds, ...)
+       call this%waterstate_tracer_inst(i)%Restart(bounds, ...)
+       call this%waterdiag_tracer_inst(i)%Restart(bounds, ...)
+       call this%waterbalance_tracer_inst(i)%Restart(bounds, ...)
     end do
   end subroutine Restart
 
