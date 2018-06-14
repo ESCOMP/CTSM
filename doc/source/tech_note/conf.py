94a19acc63557a8eb395e35372bc20cf5ba04697
Erik Kluzek <erik@ucar.edu>
2018-06-14 10:17:33 -0600

    Add a top level README file. Setup version and release in configure files. Start using versiona and release. Rather than repeat README text files, actually include them from a clm5.0 checkout, so the updated version from the code can be used.

--- doc/source/tech_note/conf.py
+++ doc/source/tech_note/conf.py
@@ -73,7 +73,11 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = []
+exclude_patterns = [
+   "manage_externals/*",
+   "README_EXTERNALS.rst",
+   "clm5.0//README.rs"
+]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
