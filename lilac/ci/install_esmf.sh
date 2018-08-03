6a3a4e435a825195cf0d19c194590f95c20e6dfe
Joseph Hamman <jhamman@ucar.edu>
2018-08-03 08:33:13 -0700

    default gfortran

--- lilac/ci/install_esmf.sh
+++ lilac/ci/install_esmf.sh
@@ -4,17 +4,15 @@ set -x
 
 cd ${HOME}
 
-export CC=""/usr/bin/gcc-6""
-export FC="/usr/bin/gfortran-6"
-export ESMF_COMPILER="/usr/bin/gfortran-6"
-alias gfortran="/usr/bin/gfortran-6"
+export FC="/usr/bin/gfortran"
+export ESMF_COMPILER="/usr/bin/gfortran"
 
 git clone https://git.code.sf.net/p/esmf/esmf deps/esmf
 cd deps/esmf
 export ESMF_DIR=$PWD
-export ESMF_INSTALL_PREFIX=/usr/esmf
-export ESMFMKFILE=${ESMF_INSTALL_PREFIX}/esmf.mk
+export ESMF_INSTALL_PREFIX=/usr/
 make -j4 lib
 make install
+export ESMFMKFILE=${ESMF_INSTALL_PREFIX}/esmf.mk
 
 cd ${TRAVIS_BUILD_DIR}
