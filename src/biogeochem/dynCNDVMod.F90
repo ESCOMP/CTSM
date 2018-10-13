78e1e288b388973886e5f48059a67a2d86f32ecd
Samuel Levis <slevis@ucar.edu>
2018-10-12 22:06:18 -0600

    Re-renamed maxpatch_pft in CNDV code
    This time renamed maxpatch_pft from natpft_size to maxsoil_patches.
    Did this in CNDV code to fix the following dv test that was FAILing:
    SMS_Ly3_Mmpi-serial.1x1_numaIA.I2000Clm50BgcDvCropQianGs.cheyenne_gnu.clm-cropMonthOutput

--- src/biogeochem/dynCNDVMod.F90
+++ src/biogeochem/dynCNDVMod.F90
@@ -81,7 +81,7 @@ contains
 
     ! Interpolate pft weight to current time step
     ! Map interpolated pctpft to subgrid weights
-    ! assumes natpft_size includes bare ground, each landunit has 1 column, 
+    ! assumes maxsoil_patches includes bare ground, each landunit has 1 column, 
     ! SCAM not defined and create_croplandunit = .false.
 
     nstep         = get_nstep()
