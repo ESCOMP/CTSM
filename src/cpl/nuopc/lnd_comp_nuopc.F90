69bc1f1e72139d5cd6de07fa3ba9ed9e40a1a7ff
Jim Edwards <jedwards@ucar.edu>
2018-12-03 09:27:05 -0700

    update nuopc interface

--- src/cpl/nuopc/lnd_comp_nuopc.F90
+++ src/cpl/nuopc/lnd_comp_nuopc.F90
@@ -554,11 +554,11 @@ contains
     call set_nextsw_cday(nextsw_cday)
 
     ! Set scalars in export state
-    call shr_nuopc_methods_State_SetScalar(dble(ldomain%ni), flds_scalar_index_nx, exportState, mpicom, &
+    call shr_nuopc_methods_State_SetScalar(dble(ldomain%ni), flds_scalar_index_nx, exportState, &
          flds_scalar_name, flds_scalar_num, rc)
     if (shr_nuopc_methods_ChkErr(rc,__LINE__,u_FILE_u)) return
 
-    call shr_nuopc_methods_State_SetScalar(dble(ldomain%nj), flds_scalar_index_ny, exportState, mpicom, &
+    call shr_nuopc_methods_State_SetScalar(dble(ldomain%nj), flds_scalar_index_ny, exportState, &
          flds_scalar_name, flds_scalar_num, rc)
     if (shr_nuopc_methods_ChkErr(rc,__LINE__,u_FILE_u)) return
 
@@ -682,7 +682,7 @@ contains
     !--------------------------------
 
     call shr_nuopc_methods_State_GetScalar(importState, &
-         flds_scalar_index_nextsw_cday, nextsw_cday, mpicom, &
+         flds_scalar_index_nextsw_cday, nextsw_cday, &
          flds_scalar_name, flds_scalar_num, rc)
     call set_nextsw_cday( nextsw_cday )
 
