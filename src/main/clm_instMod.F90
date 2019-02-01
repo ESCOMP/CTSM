f9f72e49712288b133441468618e2b505c77b8ce
negin513 <negin513@gmail.com>
2019-02-01 13:49:47 -0700

    non-constant time init for soil hydrology types are moved to SoilHydrologyType.F.

--- src/main/clm_instMod.F90
+++ src/main/clm_instMod.F90
@@ -304,9 +304,9 @@ contains
 
     call photosyns_inst%Init(bounds)
 
-    call soilhydrology_inst%Init(bounds, nlfilename)
-    call SoilHydrologyInitTimeConst(bounds, soilhydrology_inst, water_inst%waterstatebulk_inst, &
+    call soilhydrology_inst%Init(bounds, nlfilename, water_inst%waterstatebulk_inst, &
          use_aquifer_layer = use_aquifer_layer())
+    call SoilHydrologyInitTimeConst(bounds, soilhydrology_inst)
 
     call saturated_excess_runoff_inst%Init(bounds)
     call infiltration_excess_runoff_inst%Init(bounds)
