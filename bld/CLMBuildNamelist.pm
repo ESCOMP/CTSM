154bee4cc8aff90120735e631a5c35cacd321f37
Samuel Levis <slevis@ucar.edu>
2019-03-03 09:20:25 -0700

    Revision in response to code review
    Bill S caught a syntax error in my last revision

--- bld/CLMBuildNamelist.pm
+++ bld/CLMBuildNamelist.pm
@@ -2352,13 +2352,13 @@ sub setup_logic_do_transient_pfts {
    if (&value_is_true($nl->get_value($var)) && $cannot_be_true) {
       $log->fatal_error($cannot_be_true);
    }
-}
 
    if (&value_is_true($nl->get_value($var))) {
       if ($n_dom_pfts > 0 || $n_dom_landunits > 0 || $toosmall_soil > 0 || $toosmall_crop > 0 || $toosmall_glacier > 0 || $toosmall_lake > 0 || $toosmall_wetland > 0 || $toosmall_urb_tbd > 0 || $toosmall_urb_hd > 0 || $toosmall_urb_md > 0) {
          $log->fatal_error("$var cannot be combined with any of the of the following > 0: n_dom_pfts > 0, n_dom_landunit > 0, toosmall_soi > 0, toosmall_crop > 0, toosmall_glacier > 0, toosmall_lake > 0, toosmall_wetland > 0, toosmall_urb_tbd > 0, toosmall_urb_hd > 0, toosmall_urb_md > 0");
       }
    }
+}
 
 sub setup_logic_do_transient_crops {
    #
