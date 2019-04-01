e5f0c79a784c99133ca6d6c8f25fe8e3adcfb9dd
negin513 <negin513@gmail.com>
2019-04-01 10:49:35 -0600

    writing out the maximum values for all the errors. We casn see errsoi_col_max_val of 1.000000000000000E+036 (spval)  at least in ERP_P36x2_D_Ld5.f10_f10_musgs.IHistClm45BgcCruGs.cheyenne_intel.clm-decStart case. Therefore, we need the max for errsoi_col ... In EnergyFluxType.F90 errsoi_col is set to spval...

--- src/biogeophys/BalanceCheckMod.F90
+++ src/biogeophys/BalanceCheckMod.F90
@@ -421,6 +421,7 @@ contains
        end do
        
        errh2o_max_val = maxval(abs(errh2o(bounds%begc:bounds%endc)))
+       write(iulog,*)'errh2o_max_val      :', errh2o_max_val
 
        if (errh2o_max_val > h2o_warning_thresh) then
 
@@ -523,6 +524,7 @@ contains
 
 
        errh2osno_max_val = maxval( abs(errh2osno(bounds%begc:bounds%endc)))
+       write(iulog,*)'errh2osno_max_val      :', errh2osno_max_val
        
        if (errh2osno_max_val > h2o_warning_thresh) then
             indexc = maxloc( abs(errh2osno(bounds%begc:bounds%endc)), 1) + bounds%begc -1
@@ -620,6 +622,7 @@ contains
        ! Solar radiation energy balance check
 
        errsol_max_val = maxval( abs(errsol(bounds%begp:bounds%endp)), mask = (errsol(bounds%begp:bounds%endp) /= spval) ) 
+       write(iulog,*)'errsol_max_val      :', errsol_max_val
 
        if  ((errsol_max_val > energy_warning_thresh) .and. (DAnstep > skip_steps)) then
 
@@ -649,6 +652,7 @@ contains
        ! Longwave radiation energy balance check
 
        errlon_max_val = maxval( abs(errlon(bounds%begp:bounds%endp)), mask = (errlon(bounds%begp:bounds%endp) /= spval) )
+       write(iulog,*)'errlon_max_val      :', errlon_max_val
 
        if ((errlon_max_val > energy_warning_thresh) .and. (DAnstep > skip_steps)) then
             indexp = maxloc( abs(errlon(bounds%begp:bounds%endp)), 1 , mask = (errlon(bounds%begp:bounds%endp) /= spval) ) + bounds%begp -1
@@ -665,6 +669,7 @@ contains
        ! Surface energy balance check
 
        errseb_max_val = maxval( abs(errseb(bounds%begp:bounds%endp)))
+       write(iulog,*)'errseb_max_val      :', errseb_max_val
 
        if ((errseb_max_val > energy_warning_thresh) .and. (DAnstep > skip_steps)) then
 
@@ -702,6 +707,7 @@ contains
        ! Soil energy balance check
 
        errsoi_col_max_val  =  maxval( abs(errsoi_col(bounds%begc:bounds%endc)) )
+       write(iulog,*)'errsoi_col_max_val      :', errsoi_col_max_val
 
        if (errsoi_col_max_val > 1.0e-5_r8 ) then
            indexc = maxloc( abs(errsoi_col(bounds%begc:bounds%endc)), 1 ) + bounds%begc -1
