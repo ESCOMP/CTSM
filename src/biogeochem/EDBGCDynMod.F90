3b8b9338ab24f5fc32101e378a7f193b98531d9f
mwgraham <mwgraham@vt.edu>
2019-04-02 15:25:57 -0600

    added num_soilc, filter_soilc line 187 to /gpfs/u/home/mwgraham/ctsm_2/src/biogeochem/EDBGCDynMod.F90

--- src/biogeochem/EDBGCDynMod.F90
+++ src/biogeochem/EDBGCDynMod.F90
@@ -183,7 +183,7 @@ contains
     !--------------------------------------------
 
     if (use_century_decomp) then
-       call decomp_rate_constants_bgc(bounds, num_soilc, filter_soilc, &
+       call decomp_rate_constants_bgc(bounds, num_soilc, filter_soilc, num_soilp, filter_soilp &
             canopystate_inst, soilstate_inst, temperature_inst, ch4_inst, soilbiogeochem_carbonflux_inst)
     else
        call decomp_rate_constants_cn(bounds, num_soilc, filter_soilc, &
