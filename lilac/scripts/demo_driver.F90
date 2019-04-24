c1df7955a3a2de4fb4437efba51932ce7f47ed44
negin513 <negin513@gmail.com>
2019-04-24 11:07:06 -0600

    working version of the driver with the data types.

--- lilac/scripts/demo_driver.F90
+++ lilac/scripts/demo_driver.F90
@@ -1,36 +1,49 @@
 program demo_lilac_driver
 
-  ! modules
-  use ESMF
-  use Lilac_mod
+    ! modules
+    use ESMF
+    !use Lilac_mod
 
-  implicit none
+    !use lilac_utils, only   : atm2lnd_data1d_type , lnd2atm_data1d_type, atm2lnd_data2d_type, atm2lnd_data2d_type
+    !use lilac_utils, only   : atm2lnd_data2d_type
+    implicit none
 
-  real, dimension(100,100) :: dum_var1
-  real, dimension(100,100) :: dum_var2
+    type                :: atm2lnd_data1d_type
+        real*8, pointer :: uwind (:)
+        real*8, pointer :: vwind (:)
+        real*8, pointer :: tbot (:)
+    end type atm2lnd_data1d_type
 
-  !real, dimension(10) :: t_phy   ! temperature (K)
-  !real, dimension(10) :: th_phy  ! potential temperature (K)
-  !real, dimension(10,10) :: rho     !
+    type                :: lnd2atm_data1d_type
+        real*8, pointer :: lwup(:)
+        real*8, pointer :: taux(:)
+        real*8, pointer :: tauy(:)
+     end type lnd2atm_data1d_type
 
-  call random_number(dum_var1)
-  call random_number(dum_var2)
 
-  !call random_number(t_phy)
-  !call random_number(th_phy)
+    type (atm2lnd_data1d_type)                            :: atm2lnd
+    type (lnd2atm_data1d_type)                            :: lnd2atm
+    integer                                               :: begc,endc
+    real, dimension(100,100), target                      :: dum_var1
+    real, dimension(100)                                  :: dum_var2
 
-  !print *, "dum_var1 = ", dum_var1
-  !print *, "dum_var2 = ", dum_var2
+    begc = 1
+    endc = 100
 
-  call lilac_init(dum_var1, dum_var2)
-  call lilac_run(dum_var1, dum_var2)
+    call random_number(dum_var2)
+    allocate( atm2lnd%uwind           (begc:endc) )  ; atm2lnd%uwind   (:)      =  dum_var2 
+    allocate( atm2lnd%vwind           (begc:endc) )  ; atm2lnd%vwind   (:)      =  dum_var2
+    allocate( atm2lnd%tbot            (begc:endc) )  ; atm2lnd%tbot    (:)      =  dum_var2
+    allocate( lnd2atm%lwup            (begc:endc) )  ; lnd2atm%lwup    (:)      =  dum_var2
+    allocate( lnd2atm%taux            (begc:endc) )  ; lnd2atm%taux    (:)      =  dum_var2
+    allocate( lnd2atm%tauy            (begc:endc) )  ; lnd2atm%tauy    (:)      =  dum_var2
 
-  !----------------------------------------------
-  !--- Done ---
-  !----------------------------------------------
-  !write(iunit,*) subname,' DONE'
-  print *, " ... DONE ..."
-  call ESMF_Finalize()
+    print *, "======================================="
+    print *, "======================================="
+    print *, atm2lnd%uwind
+
+    print *, "======================================="
+    print *, " ... DONE ..."
 
 
 
