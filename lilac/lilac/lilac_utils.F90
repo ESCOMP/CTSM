4b4a9fe23af7ff0d14f2a26250eabb19d7ac102a
negin513 <negin513@gmail.com>
2019-08-08 12:10:27 -0600

    adding the default values for atm --> lnd

--- lilac/lilac/lilac_utils.F90
+++ lilac/lilac/lilac_utils.F90
@@ -13,6 +13,7 @@ module lilac_utils
 
     integer, parameter              :: fldsMax = 100
 
+    character(*) , parameter                          :: modname     = "lilac_utils"
     ! !PUBLIC TYPES:
     type                            :: fld_list_type
         character(len=128)          :: stdname
@@ -75,8 +76,9 @@ module lilac_utils
 
         ! local variables
         integer :: rc
-        character(len=*), parameter                :: subname=':[fldlist_add]'
+        character(len=*), parameter                :: subname=trim(modname)//':[fldlist_add]'
         !-------------------------------------------------------------------------------
+       call ESMF_LogWrite(subname//"inside fldlist_add!", ESMF_LOGMSG_INFO)
 
         ! Set up a list of field information
         num = num + 1
@@ -107,7 +109,8 @@ module lilac_utils
 
     end subroutine fldlist_add
 
-    subroutine create_fldlists(a2c_fldlist, c2l_fldlist, l2c_fldlist, c2a_fldlist, rof_prognostic, glc_present )
+    !subroutine create_fldlists(a2c_fldlist, c2l_fldlist, l2c_fldlist, c2a_fldlist, rof_prognostic, glc_present )
+    subroutine create_fldlists(a2c_fldlist, c2l_fldlist, l2c_fldlist, c2a_fldlist)
         
         ! add all the necessary fields one by one to the fieldlist 
         type(fld_list_type),        intent(inout) :: a2c_fldlist(fldsMax)
@@ -115,45 +118,67 @@ module lilac_utils
         type(fld_list_type),        intent(inout) :: l2c_fldlist(fldsMax)
         type(fld_list_type),        intent(inout) :: c2l_fldlist(fldsMax)
 
-        !type (fld_list_type)                      :: fldsToLnd(fldsMax)
-        !type (fld_list_type)                      :: fldsFrLnd(fldsMax)
-
-        !integer                                   :: fldsFrCpl_num, fldsToCpl_num
-        integer                                   :: fldsToLnd_num != 0  ! From atmosphere to land (a2c and c2l)
-        integer                                   :: fldsFrLnd_num != 0  ! From land to atmosphere (l2c and c2a)
+        ! I use this as an index!
+        integer                                   :: fldsToLnd_num != 0  ! From atmosphere to land (c2l)
+        integer                                   :: fldsFrLnd_num != 0  ! From land to atmosphere (l2c)
+        integer                                   :: fldsToAtm_num != 0  ! From land to atmosphere (c2a)
+        integer                                   :: fldsFrAtm_num != 0  ! From atmosphere to land (a2c)
         integer, parameter                        :: fldsMax = 100
 
 
-        logical                     , intent(in)  :: glc_present    ! .true. => running with a non-stub GLC model
-        logical                     , intent(in)  :: rof_prognostic ! .true. => running with a prognostic ROF model
+        ! TODO (NS) : Should we move these to the land cap????
+        logical                                   :: glc_present    ! .true. => running with a non-stub GLC model
+        logical                                   :: rof_prognostic ! .true. => running with a prognostic ROF model
 
+        character(len=*), parameter                :: subname=trim(modname)//':[create_fldlists]'
         ! TODO (NS) : I should add default value and units here.....
+
+        fldsToLnd_num= 0
+        fldsFrLnd_num= 0
+        fldsToAtm_num= 0
+        fldsFrAtm_num= 0
+
+       call ESMF_LogWrite(subname//"is called!", ESMF_LOGMSG_INFO)
 
         !-------------------------------------------------------------------------
         !            !---- from atm ----! a2c_fldlist &  c2l_fldlist
         !-------------------------------------------------------------------------
         !--------------------------a2c_fldlist------------------------------------
         ! from atm - states
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_z'         )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_topo'      )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_u'         )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_v'         )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_ptem'      )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_pbot'      )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_tbot'      )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_shum'      )
-        !call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Sa_methane'   )
+        !call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_z'         )
+        !call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_topo'      )
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_z'         ,   default_value=30.0      , units='m/s')
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_topo'      ,   default_value=10.0      , units='m')
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_u'           , default_value=0.0      , units='m/s')
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_v'           , default_value=0.0      , units='m/s')
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_ptem'        , default_value=280.0    , units='degK')
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_pbot'        , default_value=100100.0 , units='pa'  )
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_tbot'        , default_value=280.0    , units='degk' )
+        call fldlist_add(fldsToLnd_num  , a2c_fldlist , 'Sa_shum'        , default_value=0.0004   , units='kg/kg' )
+        !call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Sa_methane'   )
+
+       call ESMF_LogWrite(subname//"from atmosphere states are added!" , ESMF_LOGMSG_INFO)
+
+
+
+
 
         ! from atm - fluxes
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_lwdn'    )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_rainc'   )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_rainl'   )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_snowc'   )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_snowl'   )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_swndr'   )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_swvdr'   )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_swndf'   )
-        call fldlist_add(fldsToLnd_num, a2c_fldlist, 'Faxa_swvdf'   )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_lwdn'  , default_value=200.0  , units='W/m2'   )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_rainc' , default_value=4.0e-8 , units='kg/m2s' )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_rainl' , default_value=3.0e-8 , units='kg/m2s' )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_snowc' , default_value=1.0e-8 , units='kg/m2s' )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_snowl' , default_value=2.0e-8 , units='kg/m2s' )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_swndr' , default_value=100.0  , units='W/m2'   )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_swvdr' , default_value=90.0   , units='W/m2'   )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_swndf' , default_value=20.0   , units='W/m2'   )
+        call fldlist_add(fldsToLnd_num , a2c_fldlist , 'Faxa_swvdf' , default_value=40.0   , units='W/m2'   )
+
+        call ESMF_LogWrite(subname//"from atmosphere fluxes are added!", ESMF_LOGMSG_INFO)
+
+        ! call fldlist_add(fldsToCpl_num, fldsToCpl, 'Faxa_bcphidry')
+        ! call fldlist_add(fldsToCpl_num, fldsToCpl, 'Faxa_bcphodry')
+        ! call fldlist_add(fldsToCpl_num, fldsToCpl, 'Faxa_bcphiwet')
 
         !--------------------------c2l_fldlist------------------------------------
         ! from atm - states
@@ -166,6 +191,7 @@ module lilac_utils
         call fldlist_add(fldsToLnd_num, c2l_fldlist, 'Sa_tbot'      )
         call fldlist_add(fldsToLnd_num, c2l_fldlist, 'Sa_shum'      )
         !call fldlist_add(fldsToLnd_num, c2l_fldlist, 'Sa_methane'   )
+       call ESMF_LogWrite(subname//"from atmosphere states are added!", ESMF_LOGMSG_INFO)
 
         ! from atm - fluxes
         call fldlist_add(fldsToLnd_num, c2l_fldlist, 'Faxa_lwdn'    )
@@ -177,6 +203,7 @@ module lilac_utils
         call fldlist_add(fldsToLnd_num, c2l_fldlist, 'Faxa_swvdr'   )
         call fldlist_add(fldsToLnd_num, c2l_fldlist, 'Faxa_swndf'   )
         call fldlist_add(fldsToLnd_num, c2l_fldlist, 'Faxa_swvdf'   )
+        call ESMF_LogWrite(subname//"from atmosphere fluxes are added!", ESMF_LOGMSG_INFO)
 
         !-------------------------------------------------------------------------
         !            !---- from lnd ----! l2c_fldlist &  c2a_fldlist
@@ -195,25 +222,41 @@ module lilac_utils
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Sl_u10'        )
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Sl_fv'         )
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Sl_ram1'       )
+        call ESMF_LogWrite(subname//"l2c: from land states are added!", ESMF_LOGMSG_INFO)
 
-
+        rof_prognostic = .false.
         ! export fluxes to river
         if (rof_prognostic) then
+            call ESMF_LogWrite(subname//"Okay we are in rof_prognostic", ESMF_LOGMSG_INFO)
            call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Flrl_rofsur'   )
+            call ESMF_LogWrite(subname//"Okay we are in rof_prognostic 13", ESMF_LOGMSG_INFO)
            call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Flrl_rofgwl'   )
+            call ESMF_LogWrite(subname//"Okay we are in rof_prognostic 14", ESMF_LOGMSG_INFO)
            call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Flrl_rofsub'   )
+            call ESMF_LogWrite(subname//"Okay we are in rof_prognostic 15", ESMF_LOGMSG_INFO)
            call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Flrl_rofi'     )
+            call ESMF_LogWrite(subname//"Okay we are in rof_prognostic 16", ESMF_LOGMSG_INFO)
            call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Flrl_irrig'    )
+            call ESMF_LogWrite(subname//"l2c: from land states are added for rof_prognostic!", ESMF_LOGMSG_INFO)
         end if
 
         ! export fluxes to atm
+        call ESMF_LogWrite(subname//"l2c: now adding fluxes to atmosphere!", ESMF_LOGMSG_INFO)
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_taux'     )
+        call ESMF_LogWrite(subname//"l2c: Fall_taux!", ESMF_LOGMSG_INFO)
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_tauy'     )
+        call ESMF_LogWrite(subname//"l2c: Fall_taux!", ESMF_LOGMSG_INFO)
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_lat'      )
+        call ESMF_LogWrite(subname//"l2c: Fall_lat!", ESMF_LOGMSG_INFO)
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_sen'      )
+        call ESMF_LogWrite(subname//"l2c: Fall_sen!", ESMF_LOGMSG_INFO)
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_lwup'     )
+        call ESMF_LogWrite(subname//"l2c: Fall_lwup!", ESMF_LOGMSG_INFO)
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_evap'     )
+        call ESMF_LogWrite(subname//"l2c: Fall_evap!", ESMF_LOGMSG_INFO)
         call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_swnet'    )
+        call ESMF_LogWrite(subname//"l2c: Fall_lat!", ESMF_LOGMSG_INFO)
+        call ESMF_LogWrite(subname//"l2c: from land fluxes are added!", ESMF_LOGMSG_INFO)
 
         ! call fldlist_add(fldsFrLnd_num, l2c_fldlist, 'Fall_methane'  )
 
