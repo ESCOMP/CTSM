6704bc028e9b457c1fcbb7d3e8929c98ef85e2d9
Bill Sacks <sacks@ucar.edu>
2020-03-10 09:44:22 -0600

    Update number of expected tests in build-namelist tester

--- bld/unit_testers/build-namelist_test.pl
+++ bld/unit_testers/build-namelist_test.pl
@@ -138,9 +138,9 @@ my $testType="namelistTest";
 #
 # Figure out number of tests that will run
 #
-my $ntests = 927;
+my $ntests = 919;
 if ( defined($opts{'compare'}) ) {
-   $ntests += 588;
+   $ntests += 585;
 }
 plan( tests=>$ntests );
 
