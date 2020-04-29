6965442e6c528684bf2de1a20c31036e2bb5507c
Erik Kluzek <erik@ucar.edu>
2020-04-29 10:56:55 -0600

    Remove note about not being able to use hires option for nldas grid as now can, fix a difference that didn't get in from the release branch that wasn't allowing the code to compile in mkglcmecMod

--- tools/mksurfdata_map/src/mkglcmecMod.F90
+++ tools/mksurfdata_map/src/mkglcmecMod.F90
@@ -213,7 +213,7 @@ subroutine mkglcmec(ldomain, mapfname, &
   topoglcmec_o(:,:)         = 0.
   if ( outnc_3dglc )then
       if ( (.not. present(pctglcmec_gic_o)) .or. (.not. present(pctglcmec_icesheet_o)) .or. &
-           (.not. present(pctglc_gic_o)     .or. (.not. present(pctglc_icesheet_o) )then
+           (.not. present(pctglc_gic_o)   ) .or. (.not. present(pctglc_icesheet_o)   ) )then
          call shr_sys_abort( subname//' ERROR: 3D glacier fields were NOT sent in and they are required' )
       end if
       pctglcmec_gic_o(:,:)      = 0.
