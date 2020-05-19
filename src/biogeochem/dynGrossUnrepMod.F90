0ea4d9672115b0fca8d6c81d6837697cf9d9bb34
Erik Kluzek <erik@ucar.edu>
2020-05-19 11:39:43 -0600

    Handle ilivestemxf_to_iout_gmc reaching 1/dt like others

--- src/biogeochem/dynGrossUnrepMod.F90
+++ src/biogeochem/dynGrossUnrepMod.F90
@@ -566,8 +566,10 @@ contains
                      gru_frootc_xfer_to_atm(p)               = frootc_xfer(p) 
                      cnveg_carbonflux_inst%matrix_gmtransfer_patch(p,ifrootxf_to_iout_gmc) = 1._r8 / dtime
                   end if
-                  if(cnveg_carbonflux_inst%matrix_gmtransfer_patch(p,ilivestemxf_to_iout_gmc) .ge. 1._r8 / dtime) &
-                     call endrun(msg=' livestemxf_to_out_ph > 1/dt'//errMsg(sourcefile, __LINE__))
+                  if(cnveg_carbonflux_inst%matrix_gmtransfer_patch(p,ilivestemxf_to_iout_gmc) .ge. 1._r8 / dtime) then
+                     gru_livestemc_xfer_to_atm(p)               = livestemc_xfer(p) 
+                     cnveg_carbonflux_inst%matrix_gmtransfer_patch(p,ilivestemxf_to_iout_gmc) = 1._r8 / dtime
+                  end if
                   if(cnveg_carbonflux_inst%matrix_gmtransfer_patch(p,ideadstemxf_to_iout_gmc) .ge. 1._r8 / dtime) &
                      call endrun(msg=' deadstemxf_to_out_ph > 1/dt'//errMsg(sourcefile, __LINE__))
                   if(cnveg_carbonflux_inst%matrix_gmtransfer_patch(p,ilivecrootxf_to_iout_gmc) .ge. 1._r8 / dtime) &
