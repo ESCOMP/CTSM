db9dfc2fd683b01fd2b6cf30982488f440145c0d
Bill Sacks <sacks@ucar.edu>
2020-06-11 13:14:41 -0600

    Minor rework of lilacsmoke test
    Get it passing on my mac (needs --build-without-openmp) and a bit of
    refactoring

--- cime_config/SystemTests/lilacsmoke.py
+++ cime_config/SystemTests/lilacsmoke.py
@@ -27,7 +27,6 @@ class LILACSMOKE(SystemTestsCommon):
             exeroot = self._case.get_value('EXEROOT')
             build_dir = os.path.join(caseroot, 'lilac_build')
             script_path = os.path.abspath(os.path.join(lndroot, 'lilac', 'build_ctsm'))
-            logs = []
 
             # We only run the initial build command if the build_dir doesn't exist
             # yet. This is to support rebuilding the test case. (The first time through,
@@ -37,6 +36,10 @@ class LILACSMOKE(SystemTestsCommon):
                 machine = self._case.get_value('MACH')
                 compiler = self._case.get_value('COMPILER')
                 debug = self._case.get_value('DEBUG')
+                # It would be possible to do this testing via the python interface rather
+                # than through a separate subprocess. However, we do it through a
+                # subprocess in order to test the full build_ctsm script, including
+                # command-line parsing.
                 cmd = '{script_path} {build_dir} --machine {machine} --compiler {compiler}'.format(
                     script_path=script_path,
                     build_dir=build_dir,
@@ -44,12 +47,11 @@ class LILACSMOKE(SystemTestsCommon):
                     compiler=compiler)
                 if debug:
                     cmd += ' --build-debug'
-                append_testlog(cmd)
-                run_cmd_no_fail(cmd, arg_stdout='build_ctsm.bldlog', combine_output=True, from_dir=exeroot)
-                logfile = os.path.join(exeroot, 'build_ctsm.bldlog')
-                logs.append(logfile)
-                with open(logfile) as lf:
-                    append_testlog(lf.read())
+                # For now, always build this test without threads: it doesn't need
+                # threads, and building unthreaded ensures that it works on a wider range
+                # of machines/compilers
+                cmd += ' --build-without-openmp'
+                self._run_build_cmd(cmd, exeroot, 'build_ctsm.bldlog')
 
             # We call the build script with --rebuild even for an initial build. This is
             # so we make sure to test the code path for --rebuild. (This is also needed if
@@ -58,15 +60,28 @@ class LILACSMOKE(SystemTestsCommon):
             cmd = '{script_path} {build_dir} --rebuild'.format(
                 script_path=script_path,
                 build_dir=build_dir)
-            append_testlog(cmd)
-            run_cmd_no_fail(cmd, arg_stdout='rebuild_ctsm.bldlog', combine_output=True, from_dir=exeroot)
-            logfile = os.path.join(exeroot, 'rebuild_ctsm.bldlog')
-            logs.append(logfile)
-            with open(logfile) as lf:
-                append_testlog(lf.read())
+            self._run_build_cmd(cmd, exeroot, 'rebuild_ctsm.bldlog')
 
-            post_build(self._case, logs, build_complete=True)
+            # Setting logs=[] implies that we don't bother gzipping any of the build log
+            # files; that seems fine for these purposes (and it keeps the above code
+            # simpler).
+            post_build(self._case, logs=[], build_complete=True)
 
     def run_phase(self):
         # FIXME(wjs, 2020-06-10) Fill this in
         pass
+
+    @staticmethod
+    def _run_build_cmd(cmd, exeroot, logfile):
+        """
+        Runs the given build command, with output to the given logfile
+
+        Args:
+        cmd: str (command to run)
+        exeroot: str (path to exeroot)
+        logfile: str (path to logfile)
+        """
+        append_testlog(cmd)
+        run_cmd_no_fail(cmd, arg_stdout=logfile, combine_output=True, from_dir=exeroot)
+        with open(os.path.join(exeroot, logfile)) as lf:
+            append_testlog(lf.read())
