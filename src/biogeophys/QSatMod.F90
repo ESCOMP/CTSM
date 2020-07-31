a5a173105312b91fa51f3e17b2114acca0693b64
Bill Sacks <sacks@ucar.edu>
2020-07-31 16:46:18 -0600

    Make most QSat output arguments optional
    This has two benefits:
    
    (1) It prevents callers from needing to create dummy variables to hold
        outputs that they don't actually need.
    
    (2) If neither qsdT nor esdT are needed, then we can avoid doing some
        expensive calculations.

--- src/biogeophys/QSatMod.F90
+++ src/biogeophys/QSatMod.F90
@@ -58,10 +58,10 @@ module QSatMod
 contains
 
   !-----------------------------------------------------------------------
-  subroutine QSat (T, p, es, esdT, qs, qsdT)
+  subroutine QSat (T, p, qs, es, qsdT, esdT)
     !
     ! !DESCRIPTION:
-    ! Computes saturation mixing ratio and the change in saturation
+    ! Computes saturation mixing ratio and (optionally) the change in saturation
     ! mixing ratio with respect to temperature.
     ! Reference:  Polynomial approximations from:
     !             Piotr J. Flatau, et al.,1992:  Polynomial fits to saturation
@@ -75,49 +75,55 @@ contains
     implicit none
     real(r8), intent(in)  :: T        ! temperature (K)
     real(r8), intent(in)  :: p        ! surface atmospheric pressure (pa)
-    real(r8), intent(out) :: es       ! vapor pressure (pa)
-    real(r8), intent(out) :: esdT     ! d(es)/d(T)
     real(r8), intent(out) :: qs       ! humidity (kg/kg)
-    real(r8), intent(out) :: qsdT     ! d(qs)/d(T)
+    real(r8), intent(out), optional :: es       ! vapor pressure (pa)
+    real(r8), intent(out), optional :: qsdT     ! d(qs)/d(T)
+    real(r8), intent(out), optional :: esdT     ! d(es)/d(T)
     !
     ! !LOCAL VARIABLES:
-    real(r8) :: T_limit
+    real(r8) :: es_local    ! local version of es (in case es is not present)
+    real(r8) :: esdT_local  ! local version of esdT (in case esdT is not present)
     real(r8) :: td,vp,vp1,vp2
     !-----------------------------------------------------------------------
 
-    T_limit = T - SHR_CONST_TKFRZ
-    if (T_limit > 100.0_r8) T_limit=100.0_r8
-    if (T_limit < -75.0_r8) T_limit=-75.0_r8
+    td = T - SHR_CONST_TKFRZ
+    if (td > 100.0_r8) td=100.0_r8
+    if (td < -75.0_r8) td=-75.0_r8
 
-    td       = T_limit
     if (td >= 0.0_r8) then
-       es   = a0 + td*(a1 + td*(a2 + td*(a3 + td*(a4 &
+       es_local = a0 + td*(a1 + td*(a2 + td*(a3 + td*(a4 &
             + td*(a5 + td*(a6 + td*(a7 + td*a8)))))))
-
-       esdT = b0 + td*(b1 + td*(b2 + td*(b3 + td*(b4 &
-            + td*(b5 + td*(b6 + td*(b7 + td*b8)))))))
-
     else
-       es   = c0 + td*(c1 + td*(c2 + td*(c3 + td*(c4 &
+       es_local = c0 + td*(c1 + td*(c2 + td*(c3 + td*(c4 &
             + td*(c5 + td*(c6 + td*(c7 + td*c8)))))))
-
-       esdT = d0 + td*(d1 + td*(d2 + td*(d3 + td*(d4 &
-            + td*(d5 + td*(d6 + td*(d7 + td*d8)))))))
-
     endif
 
-    es    = es    * 100._r8            ! pa
-
-    esdT  = esdT  * 100._r8            ! pa/K
-
-
-    vp    = 1.0_r8   / (p - 0.378_r8*es)
+    es_local = es_local * 100._r8            ! pa
+    vp    = 1.0_r8   / (p - 0.378_r8*es_local)
     vp1   = 0.622_r8 * vp
-    vp2   = vp1   * vp
+    qs    = es_local * vp1             ! kg/kg
+    if (present(es)) then
+       es = es_local
+    end if
 
-    qs    = es    * vp1             ! kg/kg
-    qsdT  = esdT  * vp2 * p         ! 1 / K
+    if (present(qsdT) .or. present(esdT)) then
+       if (td >= 0.0_r8) then
+          esdT_local = b0 + td*(b1 + td*(b2 + td*(b3 + td*(b4 &
+               + td*(b5 + td*(b6 + td*(b7 + td*b8)))))))
+       else
+          esdT_local = d0 + td*(d1 + td*(d2 + td*(d3 + td*(d4 &
+               + td*(d5 + td*(d6 + td*(d7 + td*d8)))))))
+       end if
 
+       esdT_local = esdT_local * 100._r8            ! pa/K
+       vp2 = vp1 * vp
+       if (present(qsdT)) then
+          qsdT = esdT_local * vp2 * p         ! 1 / K
+       end if
+       if (present(esdT)) then
+          esdT = esdT_local
+       end if
+    end if
 
   end subroutine QSat
 
