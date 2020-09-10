52713908da8f6eab9443cfc1a2112d45e729f24b
rosiealice <rosieafisher@gmail.com>
2020-09-10 14:53:13 +0200

    added nocomp tp main/controlMod

--- src/main/controlMod.F90
+++ src/main/controlMod.F90
@@ -227,6 +227,7 @@ contains
           use_fates_ed_prescribed_phys,                 &
           use_fates_inventory_init,                     &
           use_fates_fixed_biogeog,                      &
+          use_fates_nocomp,                             &
           fates_inventory_ctrl_filename,                &
           fates_parteh_mode
     
@@ -721,6 +722,7 @@ contains
     call mpi_bcast (use_fates_ed_prescribed_phys,  1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_inventory_init, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_fixed_biogeog, 1, MPI_LOGICAL, 0, mpicom, ier)
+    call mpi_bcast (use_fates_nocomp, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (fates_inventory_ctrl_filename, len(fates_inventory_ctrl_filename), MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (fates_paramfile, len(fates_paramfile) , MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (fates_parteh_mode, 1, MPI_INTEGER, 0, mpicom, ier)
@@ -1074,6 +1076,7 @@ contains
        write(iulog, *) '    use_fates_ed_prescribed_phys = ',use_fates_ed_prescribed_phys
        write(iulog, *) '    use_fates_inventory_init = ',use_fates_inventory_init
        write(iulog, *) '    use_fates_fixed_biogeog = ', use_fates_fixed_biogeog
+       write(iulog, *) '    use_fates_nocomp = ', use_fates_nocomp
        write(iulog, *) '    fates_inventory_ctrl_filename = ',fates_inventory_ctrl_filename
     end if
   end subroutine control_print
