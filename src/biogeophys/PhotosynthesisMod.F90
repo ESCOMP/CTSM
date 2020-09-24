1434d8dcccbaed93af1e5a56187f0eb519102a38
Keith Oleson <oleson@ucar.edu>
2020-09-24 16:31:51 -0600

    Fifth round of moving parameters to parameter file for this branch.
    BFB according to the following tests:
    ERP_D_Ld5.f10_f10_musgs.IHistClm50BgcCrop.cheyenne_intel.clm-allActive
    ERS_Ly5_P144x1.f10_f10_musgs.IHistClm50BgcCrop.cheyenne_intel.clm-cropMonthOutput
    ERS_D_Ld6.f10_f10_musgs.I1850Clm45BgcCrop.cheyenne_intel.clm-clm50CMIP6frc
    
    Parameters moved or changed:
    vcmaxse_sf (did not exist previously, PhotosynthesisMod.F90)
    jmaxse_sf (did not exist previously, PhotosynthesisMod.F90)
    tpuse_sf (did not exist previously, PhotosynthesisMod.F90)
    jmax25top_sf (did not exist previously, PhotosynthesisMod.F90)

--- src/biogeophys/PhotosynthesisMod.F90
+++ src/biogeophys/PhotosynthesisMod.F90
@@ -102,6 +102,10 @@ module  PhotosynthesisMod
      real(r8) :: lmrse      ! Entropy term for lmr (J/mol/K)
      real(r8) :: tpu25ratio ! Ratio of tpu25top to vcmax25top (unitless)
      real(r8) :: kp25ratio  ! Ratio of kp25top to vcmax25top (unitless)
+     real(r8) :: vcmaxse_sf ! Scale factor for vcmaxse (unitless)
+     real(r8) :: jmaxse_sf  ! Scale factor for jmaxse (unitless)
+     real(r8) :: tpuse_sf   ! Scale factor for tpuse (unitless)
+     real(r8) :: jmax25top_sf ! Scale factor for jmax25top (unitless)
      real(r8), allocatable, public  :: krmax              (:)
      real(r8), allocatable, private :: kmax               (:,:)
      real(r8), allocatable, private :: psi50              (:,:)
@@ -743,6 +747,14 @@ contains
     call readNcdioScalar(ncid, 'tpu25ratio', subname, params_inst%tpu25ratio)
     ! Ratio of kp25top to vcmax25top (unitless)
     call readNcdioScalar(ncid, 'kp25ratio', subname, params_inst%kp25ratio)
+    ! Scale factor for vcmaxse (unitless)
+    call readNcdioScalar(ncid, 'vcmaxse_sf', subname, params_inst%vcmaxse_sf)
+    ! Scale factor for jmaxse (unitless)
+    call readNcdioScalar(ncid, 'jmaxse_sf', subname, params_inst%jmaxse_sf)
+    ! Scale factor for tpuse (unitless)
+    call readNcdioScalar(ncid, 'tpuse_sf', subname, params_inst%tpuse_sf)
+    ! Scale factor for jmax25top (unitless)
+    call readNcdioScalar(ncid, 'jmax25top_sf', subname, params_inst%jmax25top_sf)
 
   end subroutine readParams
 
@@ -1433,7 +1445,8 @@ contains
          ! Parameters derived from vcmax25top. Bonan et al (2011) JGR, 116, doi:10.1029/2010JG001593
          ! used jmax25 = 1.97 vcmax25, from Wullschleger (1993) Journal of Experimental Botany 44:907-920.
 
-         jmax25top = (2.59_r8 - 0.035_r8*min(max((t10(p)-tfrz),11._r8),35._r8)) * vcmax25top
+         jmax25top = ((2.59_r8 - 0.035_r8*min(max((t10(p)-tfrz),11._r8),35._r8)) * vcmax25top) * &
+                          params_inst%jmax25top_sf
          tpu25top  = params_inst%tpu25ratio * vcmax25top
          kp25top   = params_inst%kp25ratio * vcmax25top
 
@@ -1564,9 +1577,9 @@ contains
 
                ! Adjust for temperature
 
-               vcmaxse = 668.39_r8 - 1.07_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)
-               jmaxse  = 659.70_r8 - 0.75_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)
-               tpuse = vcmaxse
+               vcmaxse = (668.39_r8 - 1.07_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)) * params_inst%vcmaxse_sf
+               jmaxse  = (659.70_r8 - 0.75_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)) * params_inst%jmaxse_sf
+               tpuse   = (668.39_r8 - 1.07_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)) * params_inst%tpuse_sf
                vcmaxc = fth25 (params_inst%vcmaxhd, vcmaxse)
                jmaxc  = fth25 (params_inst%jmaxhd, jmaxse)
                tpuc   = fth25 (params_inst%tpuhd, tpuse)
@@ -2992,7 +3005,8 @@ contains
          ! Parameters derived from vcmax25top. Bonan et al (2011) JGR, 116, doi:10.1029/2010JG001593
          ! used jmax25 = 1.97 vcmax25, from Wullschleger (1993) Journal of Experimental Botany 44:907-920.
 
-         jmax25top = (2.59_r8 - 0.035_r8*min(max((t10(p)-tfrz),11._r8),35._r8)) * vcmax25top
+         jmax25top = ((2.59_r8 - 0.035_r8*min(max((t10(p)-tfrz),11._r8),35._r8)) * vcmax25top) * &
+                          params_inst%jmax25top_sf
          tpu25top  = params_inst%tpu25ratio * vcmax25top
          kp25top   = params_inst%kp25ratio * vcmax25top
          luvcmax25top(p) = vcmax25top
@@ -3149,9 +3163,9 @@ contains
 
                ! Adjust for temperature
 
-               vcmaxse = 668.39_r8 - 1.07_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)
-               jmaxse  = 659.70_r8 - 0.75_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)
-               tpuse = vcmaxse
+               vcmaxse = (668.39_r8 - 1.07_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)) * params_inst%vcmaxse_sf
+               jmaxse  = (659.70_r8 - 0.75_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)) * params_inst%jmaxse_sf
+               tpuse   = (668.39_r8 - 1.07_r8 * min(max((t10(p)-tfrz),11._r8),35._r8)) * params_inst%tpuse_sf
                vcmaxc = fth25 (params_inst%vcmaxhd, vcmaxse)
                jmaxc  = fth25 (params_inst%jmaxhd, jmaxse)
                tpuc   = fth25 (params_inst%tpuhd, tpuse)
