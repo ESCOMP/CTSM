9e718287ed187888f708aabe048efe662e2b08a6
Erik Kluzek <erik@ucar.edu>
2020-11-23 11:11:05 -0700

    Remove the test that expects fates with transient to fail as that mode is now enabled by Charlie

--- bld/unit_testers/build-namelist_test.pl
+++ bld/unit_testers/build-namelist_test.pl
@@ -138,7 +138,7 @@ my $testType="namelistTest";
 #
 # Figure out number of tests that will run
 #
-my $ntests = 822;
+my $ntests = 821;
 if ( defined($opts{'compare'}) ) {
    $ntests += 495;
 }
@@ -814,11 +814,6 @@ my %failtest = (
                                    },
      "createcropFalse"           =>{ options=>"-bgc bgc -envxml_dir . -no-megan",
                                      namelst=>"create_crop_landunit=.false.",
-                                     GLC_TWO_WAY_COUPLING=>"FALSE",
-                                     phys=>"clm5_0",
-                                   },
-     "useFATESWTransient"        =>{ options=>"-bgc fates -use_case 20thC_transient -envxml_dir . -no-megan -res 10x15",
-                                     namelst=>"",
                                      GLC_TWO_WAY_COUPLING=>"FALSE",
                                      phys=>"clm5_0",
                                    },
