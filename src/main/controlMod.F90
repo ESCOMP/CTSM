12a7fe64ba12ee9cc40ea2726925a1ac269ec221
Sunniva Indrehus <73106785+sunnivin@users.noreply.github.com>
2021-05-26 11:12:59 +0200

    :bug: Corrected type argument to mpi_bcast
    mpi_type corrected: `MPI_LOGICAL --> MPI_CHARACTER` for ozone_method

--- src/main/controlMod.F90
+++ src/main/controlMod.F90
@@ -636,7 +636,7 @@ contains
     call mpi_bcast (use_crop, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fertilizer, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_grainproduct, 1, MPI_LOGICAL, 0, mpicom, ier)
-    call mpi_bcast (ozone_method, 1, MPI_LOGICAL, 0, mpicom, ier)
+    call mpi_bcast (ozone_method, 1, MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (use_snicar_frc, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_vancouver, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_mexicocity, 1, MPI_LOGICAL, 0, mpicom, ier)
