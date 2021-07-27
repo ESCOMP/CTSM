cbcd321d4d2fc83e136ac0e71f511c37cad8ae48
Negin Sobhani <negin513@gmail.com>
2021-07-27 17:31:03 -0600

    adding more options and clean-up

--- tools/contrib/neon_runner.py
+++ tools/contrib/neon_runner.py
@@ -100,8 +100,7 @@ valid_neon_sites = ['ABBY','BARR','BART','BLAN',
                     'TOOL','TREE','UKFS','UNDE',
                     'WOOD','WREF','YELL'
                    ]
- 
- 
+
 def get_parser():                                                                                                                                           
     """
     Get parser object for this script.
@@ -110,6 +109,8 @@ def get_parser():
                            formatter_class=argparse.RawDescriptionHelpFormatter)
  
     parser.print_usage = parser.print_help
+
+    #parser.add_argument('--help', action=_HelpAction, help='help for help if you need some help')
  
     parser.add_argument('--neon_site',
                 help='4-letter neon site code.', 
@@ -148,7 +149,40 @@ def get_parser():
                 action="store_true", 
                 dest="all_flag",
                 default = False)
-    parser.add_argument('--start_date',
+
+
+    subparsers = parser.add_subparsers (
+                        dest='run_type',
+                        help='Three different ways to run this script.')
+
+    ad_parser = subparsers.add_parser ('ad',
+                help=''' AD spin-up options ''') 
+
+    pad_parser = subparsers.add_parser ('postad',
+                help=''' Post-AD spin-up options ''')
+
+    tr_parser = subparsers.add_parser ('transient',
+                help=''' Transient spin-up options ''')
+
+    ad_parser.add_argument ('--ad_length',
+                help='''
+                How many years to run AD spin-up
+                [default: %(default)s]
+                ''', 
+                required = False,
+                type = int, 
+                default = 200)
+
+    pad_parser.add_argument ('--postad_length',
+                help='''
+                How many years to run in post-AD mode
+                [default: %(default)s]
+                ''', 
+                required = False,
+                type = int, 
+                default = 100)
+
+    tr_parser.add_argument('--start_date',
                 help='''
                 Start date for running CTSM simulation.
                 [default: %(default)s]
@@ -158,7 +192,8 @@ def get_parser():
                 required = False,
                 type = int,
                 default = 2018)
-    parser.add_argument('--end_year',
+
+    tr_parser.add_argument('--end_year',
                 help='''
                 End year for running CTSM simulation.
                 [default: %(default)s]
@@ -168,35 +203,35 @@ def get_parser():
                 required = False,
                 type = int,
                 default = 2020)
-    #subparsers = add_subparsers (dest='run_type')
-    #ad_parser = subparsers.add_parser ('spinup')
-    parser.add_argument('--spinup',
-                help='''
-                AD spin-up
-                [default: %(default)s]
-                ''', 
-                action="store_true",
-                dest="ad_flag",
-                required = False,
-                default = True)
-    parser.add_argument('--postad',
-                help='''
-                Post-AD spin-up
-                [default: %(default)s]
-                ''', 
-                action="store_true",
-                dest="postad_flag",
-                required = False,
-                default = True)
-    parser.add_argument('--transient',
-                help='''
-                Transient
-                [default: %(default)s]
-                ''', 
-                action="store_true",
-                dest="transient_flag",
-                required = False,
-                default = True)
+
+    #parser.add_argument('--spinup',
+    #            help='''
+    #            AD spin-up
+    #            [default: %(default)s]
+    #            ''', 
+    #            action="store_true",
+    #            dest="ad_flag",
+    #            required = False,
+    #            default = True)
+    #parser.add_argument('--postad',
+    #            help='''
+    #            Post-AD spin-up
+    #            [default: %(default)s]
+    #            ''', 
+    #            action="store_true",
+    #            dest="postad_flag",
+    #            required = False,
+    #            default = True)
+    #parser.add_argument('--transient',
+    #            help='''
+    #            Transient
+    #            [default: %(default)s]
+    #            ''', 
+    #            action="store_true",
+    #            dest="transient_flag",
+    #            required = False,
+    #            default = True)
+
     parser.add_argument('--sasu','--matrix',
                 help='''
                 Matrix (SASU) spin-up
@@ -233,124 +268,10 @@ class NeonSite :
         self.end_year = end_year
         self.start_month = start_month
         self.end_month = end_month
-       
+
     def __str__(self):
         return  str(self.__class__) + '\n' + '\n'.join((str(item) + ' = ' 
                     for item in (self.__dict__)))
-
-
-class NeonCase (Case):
-    """
-    A case to encapsulate Neon single point cases.
-    ...
-    Attributes
-    ----------
-    plat : float
-        latitude
-    plon : float
-        longitude
-    site_name: str -- default = None
-        Site name
-    Methods
-    -------
-    create_tag
-        create a tag for single point which is the site name
-        or the "lon-lat" format if the site name does not exist.
-    create_domain_at_point
-        Create domain file at a single point.
-    create_landuse_at_point:
-        Create landuse file at a single point.
-    create_surfdata_at_point:
-        Create surface dataset at a single point.
-    create_datmdomain_at_point:
-        Create DATM domain file at a single point.
-    """
-
-    def __init__(self, case_root, read_only, compset="I1PtClm51Bgc", res="CLM_USRDAT"):
-        super().__init__(case_root, read_only)
-
-        self.compset =compset
-        self._compsetname =compset
-        self.res = res
-        #self._res = res
-
-
-
-
-    def __str__(self):
-        return  str(self.__class__) + '\n' + '\n'.join((str(item) + ' = ' + str(self.__dict__[item])
-                    for item in self.__dict__))
-
-    def set_compset(self):
-        self._compsetname = self.compset
-
-    def build_base_case (self,base_root, base_case_name, res, compset, overwrite,
-                        user_mods_dir):
-
-        print (">>>>> BUILDING BASE CASE...<<<<<")
-        print ('base_root:',base_root)
-
-        case_root = os.path.join(base_root,base_case_name)
-        print ('user_mods_dir:',user_mods_dir)
-        print ('case_root:',case_root)
-        if overwrite and os.path.isdir(case_root):
-            shutil.rmtree(case_root)
-            print ("Removing the case...")
-
-        print ("Creating the case....")
-
-        #a_neon_case = NeonCase(case_root, read_only=False)
-        #print (a_neon_case)
-        #if not os.path.isdir(case_root):
-        #print ("not a case")
-        #a_neon_case.create(os.path.basename(case_root), cesmroot, compset, res,
-        #            machine_name="cheyenne", driver="nuopc",
-        #            run_unsupported=True, answer="r",walltime="01:00:00")
-        #print (a_neon_case)
-
-        #self.create(os.path.basename(case_root), srcroot=cesmroot, compset_name=self.compset, grid_name=res,
-        print (self.__str__())
-        if not os.path.isdir(case_root):
-            self.create(os.path.basename(case_root), cesmroot, self.compset, res,
-                        run_unsupported=True, answer="r",walltime="04:00:00",
-                        user_mods_dirs = ["NEON/HARV"], driver="nuopc")
-
-        self.base_case = os.path.basename(case_root)
-        self.case_setup()
-
-        build.case_build(case_root, case=self, save_build_provenance=False)
-
-        #exit()
-        #with NeonCase(case_root, read_only=False) as neon_case:
-        #    print (neon_case.__str__())
-        #    if not os.path.isdir(case_root):
-        #        neon_case.create(os.path.basename(case_root), cesmroot, compset, res,
-        #                    run_unsupported=True, answer="r",walltime="04:00:00",
-        #                    user_mods_dirs = ["NEON/HARV"], driver="nuopc")
-    #def clone_base_case (self, case_root, case_name, 
-
-
-def download_file(url, fname):
-    """
-    Function to download a file.
- 
-    Args:
-        url (str): 
-            url of the file for downloading
- 
-        fname (str) : 
-            file name to save the downloaded file.
-    """
-    response = requests.get(url)
- 
-    with open(fname, 'wb') as f:
-        f.write(response.content)
- 
-    #-- Check if download status_code
-    if response.status_code == 200:
-        print('Download finished successfully for', fname,'.')
-    elif response.status_code == 404:
-        print('File '+fname+'was not available on the neon server:'+ url)
 
 
 def check_neon_listing():
@@ -400,8 +321,8 @@ def parse_neon_listing(listing_file):
         #-- check if it is a valid neon site
         if any(key in x for x in valid_neon_sites):
             site_name = key
-            print ("Valid neon site",site_name," found!")
-            #print(grouped_df.get_group(key), "\n\n")
+            logging.debug ("Valid neon site " + site_name+" found!")
+
             tmp_df = grouped_df.get_group(key)
  
             #-- filter files only ending with .nc
@@ -411,31 +332,68 @@ def parse_neon_listing(listing_file):
             tmp_df[8] = tmp_df[8].str.replace('.nc','')
  
             tmp_df2 = tmp_df[8].str.split("-",  expand=True)
-            print (tmp_df2)
-            print ("----------------")
-        
+
+            #-- figure out start_year and end_year
             start_year = tmp_df2[0].iloc[0]
             end_year = tmp_df2[0].iloc[-1]
  
+            #-- figure out start_month and end_month
             start_month = tmp_df2[1].iloc[0]  
-            end_month = tmp_df2[1].iloc[-1]  
-            print ('start_year=',start_year)
-            print ('start_year=',end_year)
-            print ('start_month=',start_month)
-            print ('end_month=',end_month)
+            end_month = tmp_df2[1].iloc[-1] 
+
+            logging.debug ('start_year='+start_year)
+            logging.debug ('start_year=',end_year)
+            logging.debug ('start_month=',start_month)
+            logging.debug ('end_month=',end_month)
  
-            a_neon_site = NeonSite(site_name, start_year, end_year, start_month, end_month)
-            print (a_neon_site)
-            available_list.append(a_neon_site)
+            neon_site = NeonSite(site_name, start_year, end_year, start_month, end_month)
+            logging.debug (neon_site)
+            available_list.append(neon_site)
  
     return available_list
 
+def download_file(url, fname):
+    """
+    Function to download a file.
+ 
+    Args:
+        url (str): 
+            url of the file for downloading
+ 
+        fname (str) : 
+            file name to save the downloaded file.
+    """
+    response = requests.get(url)
+ 
+    with open(fname, 'wb') as f:
+        f.write(response.content)
+ 
+    #-- Check if download status_code
+    if response.status_code == 200:
+        print('Download finished successfully for', fname,'.')
+    elif response.status_code == 404:
+        print('File '+fname+'was not available on the neon server:'+ url)
 
 
 #-----------------------------------
+#-- First 
+#-----------------------------------
+
 def build_base_case(base_root, base_case_name, res, compset, overwrite,
                     user_mods_dir):
+    """
+    Function for building a base_case to clone.
+    To spend less time on building ctsm for the neon cases,
+    all the other cases are cloned from this case
+ 
+    Args:
+        url (str): 
+            url of the file for downloading
+ 
+        fname (str) : 
+            file name to save the downloaded file.
 
+    """
     print(">>>>> BUILDING BASE CASE...<<<<<<")
 
     print ('base_root:',base_root)
@@ -666,56 +624,70 @@ def main():
         site_list = valid_neon_sites
 
     out_dir = args.out_dir
-    print ("out_dir:", out_dir)
+    out_dir = "/home/negins/"
+    logging.debug ("out_dir : "+ out_dir)
 
     #if (not os.path.isdir(out_dir)):
     #    os.mkdir(out_dir)
 
-    #-- check neon listing file for available data:                       
+    #-- check neon listing file for available data: 
     available_list = check_neon_listing()
 
     print (available_list)
 
-    #testing with only one site
-    #TODO: remove later
-    neon_site= "HARV"
-
-    #--  Looping over neon sites
-
-    #for neon_site in available_list:                                                                                                                 
-    #    if neon_site.name in site_list:
-     
-    #        print ("-----------------------------------")
-    #        print ("Running CTSM for neon site : ", neon_site.name)
-    #        print ("-----------------------------------")
-            
-    #        case_dir = os.path.join(out_dir, 'NEON_'+neon_site.name+"_test_0724")
-    #        print ("case_dir:",case_dir)
-
     #=================================
 
-    base_case_name = "base_case_"+neon_site
+    #-- all neon sites can be cloned from one generic case
+    #-- so no need to define a base_case for every site.
 
-    base_root = os.path.join("/home/negins/","neon_0725",base_case_name)
+    #base_case_name = "base_case_"+neon_site
+    base_case_name = "base_case_neon"
 
-    print ("base_root:",base_root)
+    base_root = os.path.join(out_dir,"neon_sims",base_case_name)
+
+    print ("base_root:", base_root)
 
     res = "CLM_USRDAT"
     compset = "I1PtClm51Bgc"
 
-    user_mods_dir = [os.path.join(cesmroot,"cime_config","usermods_dirs","NEON",neon_site)]
+    # base_case can be any neon case
+    base_neon_site = "HARV"
+    user_mods_dir = [os.path.join(cesmroot,"cime_config","usermods_dirs","NEON",base_neon_site)]
 
     overwrite=True
 
-    # -- This is the base case. All the other cases are from this
     #orig_root = build_base_case(base_root, base_case_name, res,
-    #                           compset, overwrite, user_mods_dir)
+    #                            compset, overwrite, user_mods_dir)
     orig_root = "/home/negins/neon_0725/base_case_ABBY/base_case_ABBY"
+
+    #=================================
+    #testing with only one site
+    #TODO: remove later
+    #neon_site= "HARV"
+
+    #--  Looping over neon sites
+
+    for neon_site in available_list: 
+        if neon_site.name in site_list:
+     
+            print ("-----------------------------------")
+            print ("Running CTSM for neon site : ", neon_site.name)
+            print ("-----------------------------------")
+            
+            case_dir = os.path.join(out_dir, 'NEON_'+neon_site.name+"_test_0724")
+            print ("case_dir:",case_dir)
+
+    #for site in site_list:
+    #    if any(x.name==site for x in available_list):
+    #        print (site)
+
     print ("exit:exit!")
+    exit()
 
     #TODO: Job dependency
 
-    if args.ad_flag:
+    #if args.ad_flag:
+    if (args.run_type==ad):
         print ("======================")
         print ("Running Spin-Up case")
         print ("======================")
