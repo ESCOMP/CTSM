b297561d81ea4d149a2ad1795283b298aed7aeb8
Marius Lambert <marlam@login-4.saga>
2021-09-20 19:27:18 +0200

    add hardening as a switch

--- src/main/controlMod.F90
+++ src/main/controlMod.F90
@@ -224,7 +224,7 @@ contains
           use_fates_cohort_age_tracking,                &
           use_fates_ed_prescribed_phys,                 &
           use_fates_inventory_init,                     &
-          use_fates_fixed_biogeog,                      &
+          use_fates_fixed_biogeog, use_fates_hardening, &
           fates_inventory_ctrl_filename,                &
           fates_parteh_mode
     
@@ -705,6 +705,7 @@ contains
     call mpi_bcast (fates_spitfire_mode, 1, MPI_INTEGER, 0, mpicom, ier)
     call mpi_bcast (use_fates_logging, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_planthydro, 1, MPI_LOGICAL, 0, mpicom, ier)
+    call mpi_bcast (use_fates_hardening, 1, MPI_LOGICAL, 0, mpicom, ier) !marius
     call mpi_bcast (use_fates_cohort_age_tracking, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_ed_st3, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_ed_prescribed_phys,  1, MPI_LOGICAL, 0, mpicom, ier)
@@ -1059,6 +1060,7 @@ contains
        write(iulog, *) '    fates_paramfile = ', fates_paramfile
        write(iulog, *) '    fates_parteh_mode = ', fates_parteh_mode
        write(iulog, *) '    use_fates_planthydro = ', use_fates_planthydro
+       write(iulog, *) '    use_fates_hardening = ', use_fates_hardening
        write(iulog, *) '    use_fates_cohort_age_tracking = ', use_fates_cohort_age_tracking
        write(iulog, *) '    use_fates_ed_st3 = ',use_fates_ed_st3
        write(iulog, *) '    use_fates_ed_prescribed_phys = ',use_fates_ed_prescribed_phys
