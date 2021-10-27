f4f2deae6e6be0f186a19eeb474e29ede838bb95
Sam Rabin <sam.rabin@gmail.com>
2021-10-27 11:00:29 -0600

    cropcal_interp() now writes to this patch only.

--- src/cpl/mct/cropcalStreamMod.F90
+++ src/cpl/mct/cropcalStreamMod.F90
@@ -250,10 +250,10 @@ contains
          crop_inst%sdates_thisyr(p,1) = sdat_sdate%avs(1)%rAttr(ip,ig)
 
          ! SSR TODO: Make this work with max_growingseasons_per_year > 1
-         crop_inst%n_growingseasons_thisyear_thispatch = crop_inst%sdates_thisyr(p,1) >= 0
+         crop_inst%n_growingseasons_thisyear_thispatch(p) = crop_inst%sdates_thisyr(p,1) >= 0
 
          ! Only for first sowing date of the year
-         crop_inst%next_rx_sdate = crop_inst%sdates_thisyr(p,1)
+         crop_inst%next_rx_sdate(p) = crop_inst%sdates_thisyr(p,1)
 
       end do
    end do
