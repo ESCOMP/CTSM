0ab87f7928c480dca87128b7e60042261438656b
Samuel Levis <slevisconsulting@gmail.com>
2021-10-29 16:53:24 -0600

    Trying getfloat inst. of get_config_value for the lat/lon arguments
    As far as I can tell, this simplifies without loss of functionality.

--- python/ctsm/modify_fsurdat/fsurdat_modifier.py
+++ python/ctsm/modify_fsurdat/fsurdat_modifier.py
@@ -36,23 +36,20 @@ def main ():
                                   args.cfg_path)
     fsurdat_out = get_config_value(config, section, 'fsurdat_out',
                                    args.cfg_path)
-    idealized = config.getboolean(section, 'idealized')
-    zero_nonveg = config.getboolean(section, 'zero_nonveg')
+
+    # required but fallback values available for variables omitted
+    # entirely from the .cfg file
+    idealized = config.getboolean(section, 'idealized', fallback=False)
+    zero_nonveg = config.getboolean(section, 'zero_nonveg',
+                                    fallback=False)
+
+    lnd_lat_1 = config.getfloat(section, 'lnd_lat_1', fallback=-90)
+    lnd_lat_2 = config.getfloat(section, 'lnd_lat_2', fallback=90)
+    lnd_lon_1 = config.getfloat(section, 'lnd_lon_1', fallback=0)
+    lnd_lon_2 = config.getfloat(section, 'lnd_lon_2', fallback=360)
 
     # not required: user may set these in ./modify.cfg
     # TODO More error checking that could be done here?
-    temp = get_config_value(config, section, 'lnd_lat_1', args.cfg_path)
-    lnd_lat_1 = select_value(var=temp, default=-90, type_of_var=float)
-
-    temp = get_config_value(config, section, 'lnd_lat_2', args.cfg_path)
-    lnd_lat_2 = select_value(var=temp, default=90, type_of_var=float)
-
-    temp = get_config_value(config, section, 'lnd_lon_1', args.cfg_path)
-    lnd_lon_1 = select_value(var=temp, default=0, type_of_var=float)
-
-    temp = get_config_value(config, section, 'lnd_lon_2', args.cfg_path)
-    lnd_lon_2 = select_value(var=temp, default=360, type_of_var=float)
-
     temp = get_config_value(config, section, 'dom_nat_pft', args.cfg_path,
         allowed_values=['0','1','2','3','4','5','6','7','8','9','10','11',
                         '12','13','14',CONFIG_UNSET])
