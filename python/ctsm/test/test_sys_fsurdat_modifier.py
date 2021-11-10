853dbf9dbad5fc23a8fda9eccb8e14bdf39b0174
Samuel Levis <slevisconsulting@gmail.com>
2021-11-10 16:06:19 -0700

    Early updates to test_sys_fsurdat_modifier.py
    Does not do much, yet, but runs successfully

--- python/ctsm/test/test_sys_fsurdat_modifier.py
+++ python/ctsm/test/test_sys_fsurdat_modifier.py
@@ -13,6 +13,8 @@ from subprocess import call
 
 import ctsm.unit_testing
 
+import xarray as xr
+
 
 class TestSysFsurdatModifier(unittest.TestCase):
     """System tests for fsurdat_modifier"""
@@ -28,57 +30,40 @@ class TestSysFsurdatModifier(unittest.TestCase):
         This test specifies a minimal amount of information
         """
 
-        prefix_1 = '../tools/modify_fsurdat/'
-        prefix_2 = 'test_'
+        # fsurdat_out that we expect to create
+        prefix = 'test_'
         case = 'modify_minimal'
         suffix = '.nc'
-        file_name = prefix_1 + prefix_2 + case + suffix
-        file_exists = os.path.exists(file_name)
+        file_name_out = prefix + case + suffix
 
-        if file_exists:
-            call('rm ' + file_name)
+        if os.path.exists(file_name_out):
+            os.remove(file_name_out)  # remove old file to write the new one
 
-        command = 'fsurdat_modifier'
+        # run the fsurdat_modifier tool
+        prefix_1 = '../tools/modify_fsurdat/'  # location of tool
+        tool = 'fsurdat_modifier'
+        prefix_2 = 'ctsm/test/inputdata/'  # location of .cfg and fsurdat_in
         suffix = '.cfg'
-        call(prefix_1 + command + ' ' + prefix_1 + case + suffix)
+        command = prefix_1 + tool + ' ' + prefix_2 + case + suffix
+        call(command, shell=True)
         # the critical piece of this test is that the above command
-        # doesn't generate errors; however, we also do some assertions
-        # below
+        # doesn't generate errors; however, we also do some assertions below
 
-        # ensure that ncdump of file created matches archived ncdump
-        # of original version of that file
-        call('ncdump test_modify_minimal.nc > test_modify_minimal.asc')
-        call('diff test_modify_minimal.asc test_modify_minimal_orig.asc > dif.out')
-        number_of_lines,file_name = call('wc -l dif.out')
-        self.assertTrue(number_of_lines < 11)
+        # move the generated file to _tempdir
+        file_name_final = os.path.join(self._tempdir, file_name_out)
+        shutil.move(file_name_out, file_name_final)
 
-    def test_allInfo(self):
-        """
-        This version specifies all possible information
-        """
-        prefix_1 = '../tools/modify_fsurdat/'
-        prefix_2 = 'test_'
-        case = 'modify'  # the only difference from previous test
-        suffix = '.nc'
-        file_name = prefix_1 + prefix_2 + case + suffix
-        file_exists = os.path.exists(file_name)
+        # TODO read fsurdat_in and fsurdat_out as xarray datasets and compare
+        # TODO get filename_in from .cfg using argparse rather than hardwiring
+        file_name_in = prefix_2 + \
+            'surfdata_5x5_amazon_16pfts_Irrig_CMIP6_simyr2000_c171214.nc'
+        fsurdat_in = xr.open_dataset(file_name_in)
+        fsurdat_out = xr.open_dataset(file_name_final)
 
-        if file_exists:
-            call('rm ' + file_name)
-
-        command = 'fsurdat_modifier'
-        suffix = '.cfg'
-        call(prefix_1 + command + ' ' + prefix_1 + case + suffix)
-        # the critical piece of this test is that the above command
-        # doesn't generate errors; however, we also do some assertions
-        # below
-
-        # ensure that ncdump of file created matches archived ncdump
-        # of original version of that file
-        call('ncdump test_modify.nc > test_modify.asc')
-        call('diff test_modify.asc test_modify_orig.asc > dif.out')
-        number_of_lines,file_name = call('wc -l dif.out')
-        self.assertTrue(number_of_lines < 11)
+#   def test_allInfo(self):
+#       """
+#       This version specifies all possible information
+#       """
 
 if __name__ == '__main__':
     unit_testing.setup_for_tests()
