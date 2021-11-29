98cf4f8210e95fccf6b04bd881acfcefff7ad436
Samuel Levis <slevisconsulting@gmail.com>
2021-11-29 12:22:13 -0700

    Replace prepare_cfg_file w _create_config_file in test_sys_fsurdat_mo...
    Followed the approach used in
    cime_config/SystemTests/fsurdatmodifyctsm.py that creates cfg files
    starting from the pre-existing modify_template.cfg rather than requiring
    separate pre-existing .cfg files for every test

--- python/ctsm/test/test_sys_fsurdat_modifier.py
+++ python/ctsm/test/test_sys_fsurdat_modifier.py
@@ -5,11 +5,12 @@
 """
 
 import os
+import re
 
 import unittest
 import tempfile
 import shutil
-from subprocess import call
+from subprocess import check_call
 from configparser import ConfigParser
 
 import xarray as xr
@@ -23,7 +24,15 @@ class TestSysFsurdatModifier(unittest.TestCase):
     """System tests for fsurdat_modifier"""
 
     def setUp(self):
+        self._cfg_template_path = os.path.join(path_to_ctsm_root(),
+            'tools/modify_fsurdat/modify_template.cfg')
+        testinputs_path = os.path.join(path_to_ctsm_root(),
+            'python/ctsm/test/testinputs')
+        self._fsurdat_in = os.path.join(testinputs_path,
+            'surfdata_5x5_amazon_16pfts_Irrig_CMIP6_simyr2000_c171214.nc')
         self._tempdir = tempfile.mkdtemp()
+        self._cfg_file_path = os.path.join(self._tempdir, 'modify_fsurdat.cfg')
+        self._fsurdat_out = os.path.join(self._tempdir, 'fsurdat_out.nc')
 
     def tearDown(self):
         shutil.rmtree(self._tempdir, ignore_errors=True)
@@ -33,19 +42,18 @@ class TestSysFsurdatModifier(unittest.TestCase):
         This test specifies a minimal amount of information
         """
 
-        cfg_path, fsurdat_in, fsurdat_out = self.prepare_cfg_file(
-            'modify_minimal')
+        self._create_config_file_minimal()
 
         # run the fsurdat_modifier tool
         tool_path = os.path.join(path_to_ctsm_root(),
             'tools/modify_fsurdat/fsurdat_modifier')
-        tool_command = tool_path + ' ' + cfg_path
-        call(tool_command, shell=True)
+        tool_command = tool_path + ' ' + self._cfg_file_path
+        check_call(tool_command, shell=True)
         # the critical piece of this test is that the above command
         # doesn't generate errors; however, we also do some assertions below
 
-        fsurdat_in_data = xr.open_dataset(fsurdat_in)
-        fsurdat_out_data = xr.open_dataset(fsurdat_out)
+        fsurdat_in_data = xr.open_dataset(self._fsurdat_in)
+        fsurdat_out_data = xr.open_dataset(self._fsurdat_out)
         # assert that fsurdat_out equals fsurdat_in
         self.assertTrue(fsurdat_out_data.equals(fsurdat_in_data))
 
@@ -55,49 +63,80 @@ class TestSysFsurdatModifier(unittest.TestCase):
         This version specifies all possible information
         """
 
-        cfg_path, fsurdat_in, fsurdat_out = self.prepare_cfg_file(
-            'modify_all')
+        self._create_config_file_complete()
 
         # run the fsurdat_modifier tool
         tool_path = os.path.join(path_to_ctsm_root(),
             'tools/modify_fsurdat/fsurdat_modifier')
-        tool_command = tool_path + ' --debug ' + cfg_path
-        call(tool_command, shell=True)
+        tool_command = tool_path + ' --debug ' + self._cfg_file_path
+        check_call(tool_command, shell=True)
         # the critical piece of this test is that the above command
         # doesn't generate errors; however, we also do some assertions below
 
         # compare fsurdat_out to fsurdat_in
-        fsurdat_in_data = xr.open_dataset(fsurdat_in)
-        fsurdat_out_data = xr.open_dataset(fsurdat_out)
+        fsurdat_in_data = xr.open_dataset(self._fsurdat_in)
+        fsurdat_out_data = xr.open_dataset(self._fsurdat_out)
         # assert that fsurdat_out does not equal fsurdat_in
         self.assertFalse(fsurdat_out_data.equals(fsurdat_in_data))
 
         # compare fsurdat_out to fsurdat_out_baseline
-        fsurdat_out_baseline = fsurdat_in[:-3] + '_modified' + fsurdat_in[-3:]
+        fsurdat_out_baseline = self._fsurdat_in[:-3] + '_modified' + \
+                               self._fsurdat_in[-3:]
         fsurdat_out_base_data = xr.open_dataset(fsurdat_out_baseline)
         # assert that fsurdat_out equals fsurdat_out_baseline
         self.assertTrue(fsurdat_out_data.equals(fsurdat_out_base_data))
 
 
-    def prepare_cfg_file(self, case):
-        """
-        Enter fsurdat_in and fsurdat_out paths into the .cfg file
-        using the fill_template_file function
-        """
-        testinputs_path = os.path.join(path_to_ctsm_root(),
-                                       'python/ctsm/test/testinputs')
-        fsurdat_in = os.path.join(testinputs_path,
-            'surfdata_5x5_amazon_16pfts_Irrig_CMIP6_simyr2000_c171214.nc')
-        file_name = case + '.nc'
-        fsurdat_out = os.path.join(self._tempdir, file_name)
-        substitutions = {'FSURDAT_IN': fsurdat_in,
-                         'FSURDAT_OUT': fsurdat_out}
-        file_name = case + '.cfg'
-        cfg_template_path = os.path.join(testinputs_path, file_name)
-        cfg_path = os.path.join(self._tempdir, file_name)
-        fill_template_file(cfg_template_path, cfg_path, substitutions)
+    def _create_config_file_minimal(self):
 
-        return cfg_path, fsurdat_in, fsurdat_out
+        with open (self._cfg_file_path,'w') as cfg_out:
+            with open (self._cfg_template_path,'r') as cfg_in:
+                for line in cfg_in:
+                    if re.match(r' *fsurdat_in *=', line):
+                        line = 'fsurdat_in = {}'.format(self._fsurdat_in)
+                    elif re.match(r' *fsurdat_out *=', line):
+                        line = 'fsurdat_out = {}'.format(self._fsurdat_out)
+                    cfg_out.write(line)
+
+
+    def _create_config_file_complete(self):
+
+        with open (self._cfg_file_path,'w') as cfg_out:
+            with open (self._cfg_template_path,'r') as cfg_in:
+                for line in cfg_in:
+                    if re.match(r' *fsurdat_in *=', line):
+                        line = 'fsurdat_in = {}'.format(self._fsurdat_in)
+                    elif re.match(r' *fsurdat_out *=', line):
+                        line = 'fsurdat_out = {}'.format(self._fsurdat_out)
+                    elif re.match(r' *idealized *=', line):
+                        line = 'idealized = True'
+                    elif re.match(r' *lnd_lat_1 *=', line):
+                        line = 'lnd_lat_1 = -10\n'
+                    elif re.match(r' *lnd_lat_2 *=', line):
+                        line = 'lnd_lat_2 = -7\n'
+                    elif re.match(r' *lnd_lon_1 *=', line):
+                        line = 'lnd_lon_1 = 295\n'
+                    elif re.match(r' *lnd_lon_2 *=', line):
+                        line = 'lnd_lon_2 = 300\n'
+                    elif re.match(r' *dom_nat_pft *=', line):
+                        line = 'dom_nat_pft = 1'
+                    elif re.match(r' *lai *=', line):
+                        line = 'lai = 0 1 2 3 4 5 5 4 3 2 1 0\n'
+                    elif re.match(r' *sai *=', line):
+                        line = 'sai = 1 1 1 1 1 1 1 1 1 1 1 1\n'
+                    elif re.match(r' *hgt_top *=', line):
+                        line = 'hgt_top = 5 5 5 5 5 5 5 5 5 5 5 5\n'
+                    elif re.match(r' *hgt_bot *=', line):
+                        line = 'hgt_bot = 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5\n'
+                    elif re.match(r' *soil_color *=', line):
+                        line = 'soil_color = 5'
+                    elif re.match(r' *std_elev *=', line):
+                        line = 'std_elev = 0.1'
+                    elif re.match(r' *max_sat_area *=', line):
+                        line = 'max_sat_area = 0.2'
+                    elif re.match(r' *zero_nonveg *=', line):
+                        line = 'zero_nonveg = True'
+                    cfg_out.write(line)
 
 
 if __name__ == '__main__':
