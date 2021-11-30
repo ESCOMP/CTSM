1c0f807fb378ea04f25a425ccafb553de3795122
Samuel Levis <slevisconsulting@gmail.com>
2021-11-30 13:13:46 -0700

    Unit tests for lon_range_0_to_360 in utils.py

--- python/ctsm/test/test_unit_utils.py
+++ python/ctsm/test/test_unit_utils.py
@@ -9,7 +9,7 @@ import unittest
 import os
 
 from ctsm import unit_testing
-from ctsm.utils import fill_template_file
+from ctsm.utils import fill_template_file, lon_range_0_to_360
 
 # Allow names that pylint doesn't like, because otherwise I find it hard
 # to make readable unit test names
@@ -51,6 +51,35 @@ zyzzyva
             final_contents = f.read()
 
         self.assertEqual(final_contents, expected_final_text)
+
+
+class TestUtilsLonRange0to360(unittest.TestCase):
+    """Test of utils: lon_range_0_to_360"""
+
+    def test_lonRange0To360_lonNegative(self):
+        """
+        Tests that negative inputs to lon_range_0_to_360 in the range [-180, 0)
+        get 360 added to them
+        """
+        inputs = [-180, -0.001]
+        for input in inputs:
+            result = lon_range_0_to_360(input)
+            self.assertEqual(result, input + 360)
+
+    def test_lonRange0To360_lonNotNegative(self):
+        """
+        Tests that inputs to lon_range_0_to_360 of 0 to 360 remain unchanged
+        """
+        inputs = [0, 360]
+        for input in inputs:
+            result = lon_range_0_to_360(input)
+            self.assertEqual(result, input)
+
+    def test_lonRange0To360_outOfBounds(self):
+        with self.assertRaisesRegex(SystemExit,
+                                    "lon_in needs to be in the range 0 to 360"):
+            _ = lon_range_0_to_360(361)
+
 
 if __name__ == '__main__':
     unit_testing.setup_for_tests()
