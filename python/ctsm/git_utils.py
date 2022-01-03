d509fe28d30d25438525c3f8a28b1e343fe0cf99
Negin Sobhani <negin513@gmail.com>
2022-01-03 11:35:51 -0700

    update git_utils.

--- python/ctsm/git_utils.py
+++ python/ctsm/git_utils.py
@@ -10,22 +10,57 @@ logger = logging.getLogger(__name__)
 def get_git_short_hash():
     """
     Returns Git short SHA for the currect directory.
+
+    Args:
+
+    Raises:
+
+    Returns:
+        sha (str) : git short hash for ctsm repository
     """
-    sha = subprocess.check_output(['git', '-C', path_to_ctsm_root(),
-                'rev-parse', '--short', 'HEAD']).strip().decode()
+    sha = (
+        subprocess.check_output(
+            ["git", "-C", path_to_ctsm_root(), "rev-parse", "--short", "HEAD"]
+        )
+        .strip()
+        .decode()
+    )
     return sha
 
 
 def get_git_long_hash():
     """
     Returns Git long SHA for the currect directory.
+
+    Args:
+
+    Raises:
+
+    Returns:
+        sha (str) : git long hash for ctsm repository
     """
-    sha = subprocess.check_output(["git", '-C', path_to_ctsm_root(), "rev-parse", "HEAD"]).strip().decode()
+    sha = (
+        subprocess.check_output(["git", "-C", path_to_ctsm_root(), "rev-parse", "HEAD"])
+        .strip()
+        .decode()
+    )
     return sha
+
 
 def get_git_describe():
     """
-    Returns git describe output
+    Function for giving the recent tag of the git repo
+
+    Args:
+
+    Raises:
+
+    Returns:
+        label (str) : ouput of running 'git describe' in shell
     """
-    label = subprocess.check_output(["git", "describe"]).strip().decode()
+    label = (
+        subprocess.check_output(["git", "describe", path_to_ctsm_root()])
+        .strip()
+        .decode()
+    )
     return label
