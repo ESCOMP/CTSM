d09113dfcca24cc3c539a62443f24a338168e615
Bill Sacks <sacks@ucar.edu>
2022-01-30 12:04:03 -0700

    Fix some documentation

--- python/ctsm/git_utils.py
+++ python/ctsm/git_utils.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 
 def get_ctsm_git_short_hash():
     """
-    Returns Git short SHA for the currect directory.
+    Returns Git short SHA for the CTSM repository.
 
     Args:
 
@@ -30,7 +30,7 @@ def get_ctsm_git_short_hash():
 
 def get_ctsm_git_long_hash():
     """
-    Returns Git long SHA for the currect directory.
+    Returns Git long SHA for the CTSM repository.
 
     Args:
 
@@ -49,14 +49,14 @@ def get_ctsm_git_long_hash():
 
 def get_ctsm_git_describe():
     """
-    Function for giving the recent tag of the git repo
+    Function for giving the recent tag of the CTSM repository
 
     Args:
 
     Raises:
 
     Returns:
-        label (str) : ouput of running 'git describe' in shell
+        label (str) : ouput of running 'git describe' for the CTSM repository
     """
     label = (
         subprocess.check_output(["git", "-C", path_to_ctsm_root(), "describe"])
