0e71cb2f30b850569b2f7ab95644c4cf743e47c5
Erik Kluzek <erik@ucar.edu>
2022-02-19 17:19:18 -0700

    Correct subscript

--- src/biogeophys/LakeFluxesMod.F90
+++ src/biogeophys/LakeFluxesMod.F90
@@ -353,7 +353,7 @@ contains
             case ('MeierXXXX') 
                if(use_z0m_snowmelt) then
                   if ( snomelt_accum(c) < 1.e-5_r8 )then
-                     z0mg(c) = exp(1.4_r8 * -rpi/2.0_r8 -0.31_r8) / 1000._r8 
+                     z0mg(p) = exp(1.4_r8 * -rpi/2.0_r8 -0.31_r8) / 1000._r8 
                   else
                      z0mg(p) = exp(1.4_r8 * (atan((log10(snomelt_accum(c))+0.23_r8)/0.08_r8))-0.31_r8) / 1000._r8 
                   end if
