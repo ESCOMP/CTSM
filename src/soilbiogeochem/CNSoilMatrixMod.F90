70463e1002bc155a99d3ff1b95bb3b68d1f62973
Erik Kluzek <erik@ucar.edu>
2022-03-21 12:30:12 -0600

    rf_decomp_cascade and pathfrac_decomp_cascade were both moved to carbonflux, this is required to compile

--- src/soilbiogeochem/CNSoilMatrixMod.F90
+++ src/soilbiogeochem/CNSoilMatrixMod.F90
@@ -159,8 +159,8 @@ contains
     cascade_receiver_pool         => decomp_cascade_con%cascade_receiver_pool             ,&!Input:[integer(:)]which pool is C added to for a given decomposition step 
     floating_cn_ratio_decomp_pools=> decomp_cascade_con%floating_cn_ratio_decomp_pools    ,&!Input:[logical(:)]TRUE => pool has fixed C:N ratio
     initial_cn_ratio              => decomp_cascade_con%initial_cn_ratio                  ,&!Input:[real(r8)(:)]c:n ratio for initialization of pools
-    rf_decomp_cascade             => soilbiogeochem_state_inst%rf_decomp_cascade_col      ,&!Input:[real(r8)(:,:,:)]respired fraction in decomposition step (frac)
-    pathfrac_decomp_cascade       => soilbiogeochem_state_inst%pathfrac_decomp_cascade_col,&!Input:[real(r8)(:,:,:)]what fraction of C leaving a given pool passes 
+    rf_decomp_cascade             => soilbiogeochem_carbonflux_inst%rf_decomp_cascade_col      ,&!Input:[real(r8)(:,:,:)]respired fraction in decomposition step (frac)
+    pathfrac_decomp_cascade       => soilbiogeochem_carbonflux_inst%pathfrac_decomp_cascade_col,&!Input:[real(r8)(:,:,:)]what fraction of C leaving a given pool passes 
                                                                                             !                       through a given transition (frac)
     is_cwd                        => decomp_cascade_con%is_cwd                            ,&!Input:[logical(:)]TRUE => pool is a cwd pool
     is_litter                     => decomp_cascade_con%is_litter                         ,&!Input:[logical(:)]TRUE => pool is a litter pool
