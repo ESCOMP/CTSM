ca8a7a92b181c3d972b98863304c608ccd155e85
Erik Kluzek <erik@ucar.edu>
2022-05-02 12:00:43 -0600

    Get build script working again

--- tools/mksurfdata_esmf/gen_mksurfdata_build.sh
+++ tools/mksurfdata_esmf/gen_mksurfdata_build.sh
@@ -1,17 +1,14 @@
 #! /bin/bash -f
 
-module load cmake
-
 export MACH="cheyenne"
 export COMPILER="intel"
-export OS="LINUX"
-export ESMFMKFILE="/glade/work/oehmke/ESMF/scalable_mesh_from_file/lib/libg/Linux.intel.64.mpt.default/esmf.mk"
+export MPILIB="mpt"
 
 cwd=`pwd`
 rm -rf bld
 mkdir bld
 cd bld
-../../../cime/tools/configure --macros-format CMake --machine cheyenne --compiler intel --mpilib mpt
+../../../cime/tools/configure --macros-format CMake --machine $MACH --compiler $COMPILER --mpilib $MPILIB
 ls -l
 source ./.env_mach_specific.sh
 CC=mpicc FC=mpif90 cmake -DCMAKE_BUILD_TYPE=debug ../src
