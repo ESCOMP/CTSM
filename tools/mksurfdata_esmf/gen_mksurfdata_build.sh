333fb214c2ce8de13330f500ebd59b75441227da
Erik Kluzek <erik@ucar.edu>
2022-05-02 13:07:26 -0600

    Make some changes to the README and build script that Sam and I discussed

--- tools/mksurfdata_esmf/gen_mksurfdata_build.sh
+++ tools/mksurfdata_esmf/gen_mksurfdata_build.sh
@@ -1,16 +1,20 @@
 #! /bin/bash -f
 
+# Define what machine to use that's been ported to cime
 export MACH="cheyenne"
 export COMPILER="intel"
 export MPILIB="mpt"
 
+# Create a build directory
 cwd=`pwd`
 rm -rf bld
 mkdir bld
 cd bld
+# Run the cime configure tool to figure out what modules need to be loaded
 ../../../cime/tools/configure --macros-format CMake --machine $MACH --compiler $COMPILER --mpilib $MPILIB
-ls -l
 source ./.env_mach_specific.sh
+# Build the cmake files
 CC=mpicc FC=mpif90 cmake -DCMAKE_BUILD_TYPE=debug ../src
+# Build the actual executable
 make VERBOSE=1
 
