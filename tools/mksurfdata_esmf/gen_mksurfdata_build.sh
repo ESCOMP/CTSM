668bbd692c3dd9b23c4c2ee1d7bc5cd79e3d3d2f
Erik Kluzek <erik@ucar.edu>
2022-05-02 18:52:35 -0600

    Add handling for specifying MPILIB and COMPILER as env variables and abort if the PIO env variable is not set after configure

--- tools/mksurfdata_esmf/gen_mksurfdata_build.sh
+++ tools/mksurfdata_esmf/gen_mksurfdata_build.sh
@@ -27,13 +27,25 @@ cd bld
 # Run the cime configure tool to figure out what modules need to be loaded
 echo "Run cime configure for machine $MACH..."
 # You can specify the non-default compiler and mpi-library by adding --compiler and --mpilib settings
-../../../cime/tools/configure --macros-format CMake --machine $MACH
+if [ -z "$COMPILER" ] && [ -z "$MPILIB" ]; then
+  echo "configure for the default MPI-library and compiler..."
+  ../../../cime/tools/configure --macros-format CMake --machine $MACH
+else
+  echo "configure for the specific MPILIB=$MPILIB and COMPILER=$COMPILER..."
+  ../../../cime/tools/configure --macros-format CMake --machine $MACH --compiler $COMPILER --mpilib $MPILIB
+fi
+
 if [ $? != 0 ]; then
   echo "Error doing configure for machine name: $MACH"
   exit 1
 fi
 . .env_mach_specific.sh
 echo "COMPILER = $COMPILER, MPILIB = $MPILIB, DEBUG = $DEBUG, OS = $OS"
+if [ -z "$PIO" ]; then
+  echo "The PIO directory for the PIO build is required and was not set in the configure"
+  echo "Make sure a PIO build is provided for $MACH_$COMPILER with $MPILIB in config_machines"
+  exit 1
+fi
 # Build the cmake files
 echo "Do the cmake build..."
 CC=mpicc FC=mpif90 cmake -DCMAKE_BUILD_TYPE=debug ../src
@@ -49,4 +61,3 @@ if [ $? != 0 ]; then
   exit 1
 fi
 echo "\n\n\nSuccessfully created mksurfdata_esmf executable for: ${MACH}_${COMPILER} for $MPILIB library"
-
