9621dd6d9873ad98b7a18af2aed39c124c91897f
Samuel Levis <slevisconsulting@gmail.com>
2022-05-18 11:17:03 -0600

    Update some comments

--- cime_config/SystemTests/mksurfdataesmf.py
+++ cime_config/SystemTests/mksurfdataesmf.py
@@ -1,6 +1,10 @@
 """
 This test passes if mksurfdata_esmf generates an fsurdat (surface dataset)
 and the CTSM completes a simulation with this fsurdat file.
+
+We test res = '10x15' because it uses a lower-res topography file instead of
+the 1-km topography raw dataset. The 1-km file causes the test to run out of
+memory on cheyenne.
 """
 
 import os
@@ -20,11 +24,11 @@ class MKSURFDATAESMF(SystemTestsCommon):
         """
         SystemTestsCommon.__init__(self, case)
 
-        # Paths and strings
+        # Paths and strings needed throughout
         self._ctsm_root = self._case.get_value('COMP_ROOT_DIR_LND')
         self._tool_path = os.path.join(self._ctsm_root, 'tools/mksurfdata_esmf')
         _time_stamp = datetime.today().strftime("%y%m%d")
-        self._res = '10x15'
+        self._res = '10x15'  # see important comment in script's docstring
         self._model_yr = '1850'
         self._fsurdat_out_prefix = os.path.join(self._get_caseroot(), f'surfdata_{self._res}_hist_78pfts_CMIP6_{self._model_yr}_c{_time_stamp}.')
 
@@ -32,14 +36,13 @@ class MKSURFDATAESMF(SystemTestsCommon):
         """
         Build executable that will generate fsurdat
         Generate namelist for generating fsurdat
-        SKIP Generate jobscript
-        Submit jobscript to generate fsurdat
+        SKIP Generate jobscript that runs executable
         Modify user_nl_clm to point to the generated fsurdat
         """
         # These steps need only happen once
         if not os.path.exists(os.path.join(self._get_caseroot(),
             'done_MKSURFDATAESMF_setup.txt')):
-            # Paths and command strings
+            # Paths and strings
             self._rm_bld_dir = f"rm -rf {self._tool_path}/bld"
             self._build_script_path = os.path.join(self._tool_path,
                 'gen_mksurfdata_build.sh')
@@ -51,7 +54,7 @@ class MKSURFDATAESMF(SystemTestsCommon):
             subprocess.check_call(self._rm_bld_dir, shell=True)
             subprocess.check_call(self._build_script_path, shell=True)
 
-            # Generate namelist self._nml_script_path
+            # Generate namelist for generating fsurdat
             subprocess.check_call(self._gen_mksurfdata_namelist, shell=True)
 
             # Modify user_nl_clm to point to the generated fsurdat
@@ -63,14 +66,14 @@ class MKSURFDATAESMF(SystemTestsCommon):
 
     def run_phase(self):
         """
-        Submit jobscript to generate fsurdat
+        Run executable to generate fsurdat
         Submit CTSM run that uses fsurdat just generated
         """
         # Paths and command strings
         self._executable_path = os.path.join(self._tool_path, 'bld/mksurfdata')
         self._mpiexec_mpt_cmd = f"mpiexec_mpt -np 144 {self._executable_path} < {self._fsurdat_out_prefix}namelist"
 
-        # Submit jobscript to generate fsurdat
+        # Run executable to generate fsurdat
         subprocess.check_call(self._mpiexec_mpt_cmd, shell=True)
 
         # Submit CTSM run that uses fsurdat just generated
