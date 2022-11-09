52a0179d98d5dbd9292aeba38e4d14c952752e13
Erik Kluzek <erik@ucar.edu>
2022-11-08 23:04:01 -0700

    Add ability of add_tag_to_filename to replace resolution for the surface dataset, and add a unit test for it

--- python/ctsm/test/test_unit_utils_add_tag.py
+++ python/ctsm/test/test_unit_utils_add_tag.py
@@ -5,9 +5,9 @@
 
 import unittest
 
-from ctsm import unit_testing
 from unittest.mock import patch
 from datetime import date
+from ctsm import unit_testing
 
 from ctsm import utils
 
@@ -32,9 +32,12 @@ class TestUtilsAddTag(unittest.TestCase):
             mock_date.today.side_effect = self._fake_today
 
             fsurf_out = utils.add_tag_to_filename(fsurf_in, "tag")
+            fsurf_out2 = utils.add_tag_to_filename(fsurf_in, "tag", replace_res=True)
 
         expect_fsurf = "surfdata_0.9x1.25_hist_16pfts_Irrig_CMIP6_simyr2000_tag_c221031.nc"
         self.assertEqual(expect_fsurf, fsurf_out, "Expect filenames to be as expected")
+        expect_fsurf2 = "surfdata_tag_hist_16pfts_Irrig_CMIP6_simyr2000_c221031.nc"
+        self.assertEqual(expect_fsurf2, fsurf_out2, "Expect filenames to be as expected")
 
     def testSimpleLanduse(self):
         """Simple test of landuse dataset name"""
@@ -45,7 +48,9 @@ class TestUtilsAddTag(unittest.TestCase):
 
             landuse_out = utils.add_tag_to_filename(landuse_in, "tag")
 
-        expect_landuse = "landuse.timeseries_0.9x1.25_hist_78pfts_CMIP6_simyr1850-2015_tag_c221031.nc"
+        expect_landuse = (
+            "landuse.timeseries_0.9x1.25_hist_78pfts_CMIP6_simyr1850-2015_tag_c221031.nc"
+        )
         self.assertEqual(expect_landuse, landuse_out, "Expect filenames to be as expected")
 
     def testSimpleDatmDomain(self):
@@ -71,6 +76,7 @@ class TestUtilsAddTag(unittest.TestCase):
 
         expect_filename = "domain.lnd.fv0.9x1.25_gx1v7_tag_c221031.nc"
         self.assertEqual(expect_filename, file_out, "Expect filenames to be as expected")
+
 
 if __name__ == "__main__":
     unit_testing.setup_for_tests()
