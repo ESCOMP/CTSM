0b6cb63907ed3693b4c0c57ba7e9f728e295580f
Sabin Taranu <sabin.taranu@vub.be>
2023-03-24 15:52:15 +0100

    Add variable for sector water surface data preparation.

--- tools/mksurfdata_map/src/mkvarctl.F90
+++ tools/mksurfdata_map/src/mkvarctl.F90
@@ -50,6 +50,8 @@ module mkvarctl
   character(len=256), public :: mksrf_fvocef     = ' '  ! VOC Emission Factor data file name
   character(len=256), public :: mksrf_ftopostats = ' '  ! topography statistics data file name
   character(len=256), public :: mksrf_fvic       = ' '  ! VIC parameters data file name
+  character(len=256), public :: mksrf_fsectorwater = ' '  ! Sectoral water withdrawal and consumption data file name
+
 
   integer           , public :: numpft         = 16   ! number of plant types
 
@@ -73,6 +75,7 @@ module mkvarctl
   character(len=256), public :: map_fharvest    = ' ' ! Mapping file for harvesting
   character(len=256), public :: map_ftopostats  = ' ' ! Mapping file for topography statistics
   character(len=256), public :: map_fvic        = ' ' ! Mapping file for VIC parameters
+  character(len=256), public :: map_fsectorwater = ' ' ! Mapping file for sectoral water withdrawal and consumption data
   character(len=80) , public :: gitdescribe     = ' ' ! Description of model version from git
 !
 ! Variables to override data read in with
