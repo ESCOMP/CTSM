01d7ac51f39e3c9880cb90172860f0ffdc68a937
Erik Kluzek <erik@ucar.edu>
2023-04-06 20:45:20 -0600

    Remove plotting

--- test/tools/test_driver.sh
+++ test/tools/test_driver.sh
@@ -372,8 +372,8 @@ else
     fi
 fi
 
-# Setup conda environement
-conda activate ctsm_pylib_wdask
+# Setup conda environment
+conda activate ctsm_pylib
 if [ \$? -ne 0 ]; then
    echo "ERROR: Trouble activating the ctsm_pylib conda environment, be sure it's setup with \$CLM_ROOT/py_env_create, then rerun"
    exit 4
