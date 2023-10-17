cda0cf1412212e6f4363e6e8eb39f74c944b454d
Erik Kluzek <erik@ucar.edu>
2023-10-17 17:11:09 -0600

    Black format

--- python/ctsm/test/test_sys_gen_mksurfdata_jobscript_single.py
+++ python/ctsm/test/test_sys_gen_mksurfdata_jobscript_single.py
@@ -29,7 +29,9 @@ class TestSysGenMkSurfJSSingle(unittest.TestCase):
         os.chdir(self._tempdir)
         self.outfile = "jobscript.sh"
         self.namelist = "res.namelist"
-        bld_path = os.path.join( path_to_ctsm_root(), "python", "ctsm", "test", "testinputs", "mksurfdata_esmf_bld" )
+        bld_path = os.path.join(
+            path_to_ctsm_root(), "python", "ctsm", "test", "testinputs", "mksurfdata_esmf_bld"
+        )
         sys.argv = [
             "gen_mksurfdata_jobscript_single",
             "--bld-path",
