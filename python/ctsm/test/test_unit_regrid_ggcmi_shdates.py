96b88aa038486f6b6130f025fbc3c23dccac856d
Sam Rabin <sam.rabin@gmail.com>
2023-11-07 10:46:41 -0700

    Add unit tests for importing 2-d latitude.

--- python/ctsm/test/test_unit_regrid_ggcmi_shdates.py
+++ python/ctsm/test/test_unit_regrid_ggcmi_shdates.py
@@ -10,13 +10,16 @@ import sys
 import shutil
 
 import tempfile
-from configparser import ConfigParser
 import xarray as xr
+import numpy as np
+
+# -- add python/ctsm  to path (needed if we want to run test stand-alone)
+_CTSM_PYTHON = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, os.pardir)
+sys.path.insert(1, _CTSM_PYTHON)
 
 from ctsm import unit_testing
 from ctsm.path_utils import path_to_ctsm_root
-from ctsm.crop_calendars.regrid_ggcmi_shdates import import_coord_1d
-from ctsm.crop_calendars.regrid_ggcmi_shdates import import_coord_2d
+from ctsm.utils import import_coord_1d, import_coord_2d
 
 # Allow test names that pylint doesn't like; otherwise hard to make them
 # readable
@@ -35,41 +38,11 @@ class TestRegridGgcmiShdates(unittest.TestCase):
     def setUp(self):
         """Setup for trying out the methods"""
         testinputs_path = os.path.join(path_to_ctsm_root(), "python/ctsm/test/testinputs")
-        self._cfg_file_path = os.path.join(testinputs_path, "modify_fsurdat_opt_sections.cfg")
         self._testinputs_path = testinputs_path
-        self._fsurdat_in = os.path.join(
-            testinputs_path,
-            "surfdata_5x5_amazon_16pfts_Irrig_CMIP6_simyr2000_c171214.nc",
-        )
         self._tempdir = tempfile.mkdtemp()
-        self._fsurdat_in = os.path.join(
-            testinputs_path,
-            "surfdata_5x5_amazon_16pfts_Irrig_CMIP6_simyr2000_c171214.nc",
-        )
-        self._fsurdat_out = os.path.join(self._tempdir, "fsurdat_out.nc")
-        sys.argv = [
-            "fsurdat_modifier",
-            self._cfg_file_path,
-            "-i",
-            self._fsurdat_in,
-            "-o",
-            self._fsurdat_out,
-        ]
-        parser = fsurdat_modifier_arg_process()
-        self.cfg_path = str(parser.cfg_path)
-        self.config = ConfigParser()
-        self.config.read(self.cfg_path)
-        my_data = xr.open_dataset(self._fsurdat_in)
-        self.modify_fsurdat = ModifyFsurdat(
-            my_data=my_data,
-            lon_1=0.0,
-            lon_2=360.0,
-            lat_1=90.0,
-            lat_2=90.0,
-            landmask_file=None,
-            lat_dimname=None,
-            lon_dimname=None,
-        )
+        
+        self._1d_lonlat_file = os.path.join(self._testinputs_path, "cropcals", "swh_rf_ggcmi_crop_calendar_phase3_v1.01.nc4")
+        self._2d_lonlat_file = os.path.join(self._testinputs_path, "surfdata_5x5_amazon_16pfts_Irrig_CMIP6_simyr2000_c171214_modified.nc")
 
     def tearDown(self):
         """
@@ -77,19 +50,52 @@ class TestRegridGgcmiShdates(unittest.TestCase):
         """
         shutil.rmtree(self._tempdir, ignore_errors=True)
 
-    # def test_subgrid_and_idealized_fails(self):
-    #     # Test that 
-    #     section = "modify_fsurdat_basic_options"
-    #     self.config.set(section, "idealized", "True")
-    #     self.config.set(section, "include_nonveg", "False")
-    #     self.config.set(section, "process_subgrid_section", "True")
-    #     self.config.set(section, "dom_pft", "UNSET")
-    #     with self.assertRaisesRegex(
-    #         SystemExit,
-    #         "idealized AND process_subgrid_section can NOT both be on, pick one or the other",
-    #     ):
-    #         read_cfg_option_control(self.modify_fsurdat, self.config, section, self.cfg_path)
 
+    def test_importcoord1d_too_many_dims(self):
+        ds = xr.open_dataset(self._2d_lonlat_file)
+        with self.assertRaisesRegex(
+            SystemExit,
+            "Expected 1 dimension for LATIXY; found 2: \('lsmlat', 'lsmlon'\)",
+        ):
+            import_coord_1d(ds, "LATIXY")
+
+    def test_importcoord2d(self):
+        ds = xr.open_dataset(self._2d_lonlat_file)
+        lat, _ = import_coord_2d(ds, "lat", "LATIXY")
+        expected_values = np.array([-13.9, -11.7, -9.5, -7.3, -5.1]).astype(np.float32)
+        np.testing.assert_array_equal(lat.values, expected_values)
+
+    def test_importcoord2d_attrs(self):
+        ds = xr.open_dataset(self._2d_lonlat_file)
+        lat, _ = import_coord_2d(ds, "lat", "LATIXY")
+        expected_attributes = {
+            "long_name": "coordinate latitude",
+            "units": "degrees_north",
+        }
+        self.assertDictEqual(lat.attrs, expected_attributes)
+    
+    def test_importcoord2d_rename_dim(self):
+        ds = xr.open_dataset(self._2d_lonlat_file)
+        lat, _ = import_coord_2d(ds, "lat", "LATIXY")
+        self.assertTupleEqual(lat.dims, ("lat",))
+    
+    def test_importcoord2d_no_dim_contains_coordName(self):
+        ds = xr.open_dataset(self._2d_lonlat_file)
+        ds = ds.rename({"lsmlat": "abc"})
+        with self.assertRaisesRegex(
+            SystemExit,
+            "ERROR: Expected 1 dimension name containing lat; found 0: \[\]",
+        ):
+            import_coord_2d(ds, "lat", "LATIXY")
+    
+    def test_importcoord2d_1_dim_containing(self):
+        ds = xr.open_dataset(self._2d_lonlat_file)
+        ds = ds.rename({"lsmlon": "lsmlat2"})
+        with self.assertRaisesRegex(
+            SystemExit,
+            "Expected 1 dimension name containing lat; found 2: \['lsmlat', 'lsmlat2'\]",
+        ):
+            import_coord_2d(ds, "lat", "LATIXY")
 
 
 if __name__ == "__main__":
