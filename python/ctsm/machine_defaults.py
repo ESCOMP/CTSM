12057ea300fe7590ecd96ec8669df22377356dbf
Erik Kluzek <erik@ucar.edu>
2023-12-04 02:08:16 -0700

    Update /glade/p paths

--- python/ctsm/machine_defaults.py
+++ python/ctsm/machine_defaults.py
@@ -47,7 +47,7 @@ MACHINE_DEFAULTS = {
     "cheyenne": MachineDefaults(
         job_launcher_type=JOB_LAUNCHER_QSUB,
         scratch_dir=os.path.join(os.path.sep, "glade", "scratch", get_user()),
-        baseline_dir=os.path.join(os.path.sep, "glade", "p", "cgd", "tss", "ctsm_baselines"),
+        baseline_dir=os.path.join(os.path.sep, "glade", "p", "cgd", "tss", "To_Be_Safely_Deleted", "ctsm_baselines"),
         account_required=True,
         create_test_retry=0,
         # NOTE(wjs, 2022-02-23) By default, use the regular queue, even for
