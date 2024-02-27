23eb33be6eb1a3f681d4492f4376ac816959446f
Erik Kluzek <erik@ucar.edu>
2024-02-26 19:09:24 -0700

    Add comment about dust_emis_method

--- src/main/controlMod.F90
+++ src/main/controlMod.F90
@@ -205,7 +205,13 @@ contains
          crop_fsat_equals_zero, for_testing_run_ncdiopio_tests, &
          for_testing_use_second_grain_pool, for_testing_use_repr_structure_pool, &
          for_testing_no_crop_seed_replenishment, &
-         z0param_method, use_z0m_snowmelt, dust_emis_method
+         z0param_method, use_z0m_snowmelt 
+
+    ! NOTE: EBK 02/26/2024: dust_emis_method is here in CTSM temporarily until it's moved to CMEPS
+    ! See: https://github.com/ESCOMP/CMEPS/pull/429
+    ! Normally this should also need error checking and a broadcast, but since
+    ! there is only one hardcoded option right now that is unneeded.
+    namelist /clm_inparm/ dust_emis_method
 
     ! vertical soil mixing variables
     namelist /clm_inparm/  &
