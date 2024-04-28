0191278f95a35f90c76b7922fb51d8e42d68b24e
Sam Rabin <sam.rabin@gmail.com>
2024-04-28 13:48:21 -0600

    Add missing potential veg to build list.
    Manually cherry-picked from Greg Lemieux's b33a722.

--- bld/CLMBuildNamelist.pm
+++ bld/CLMBuildNamelist.pm
@@ -784,7 +784,8 @@ sub setup_cmdl_fates_mode {
        my @list  = (  "fates_spitfire_mode", "use_fates_planthydro", "use_fates_ed_st3", "use_fates_ed_prescribed_phys",
                       "use_fates_cohort_age_tracking","use_fates_inventory_init","use_fates_fixed_biogeog",
                       "use_fates_nocomp","use_fates_sp","fates_inventory_ctrl_filename","fates_harvest_mode",
-                      "fates_parteh_mode","use_fates_tree_damage","fates_seeddisp_cadence","use_fates_luh","fluh_timeseries","flandusepftdat");
+                      "fates_parteh_mode","use_fates_tree_damage","fates_seeddisp_cadence","use_fates_luh","fluh_timeseries","flandusepftdat",
+                      "use_fates_potentialveg");
        # dis-allow fates specific namelist items with non-fates runs
        foreach my $var ( @list ) {
           if ( defined($nl->get_value($var)) ) {
