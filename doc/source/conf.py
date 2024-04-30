cf57f65e2608471509ba26ad2283de200b261eb0
Sam Rabin <samrabin@ucar.edu>
2024-04-30 07:40:51 -0600

    Update intersphinx_mapping to new format.
    See https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_mapping

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -178,7 +178,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/', None)}
 
 numfig = True
 numfig_format = {'figure': 'Figure %s',
