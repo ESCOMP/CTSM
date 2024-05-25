175a3d01082437945f15660f75aca860c626d0a4
Gregory Lemieux <glemieux@users.noreply.github.com>
2024-05-24 18:14:18 -0600

    pvt system test setup looks correct

--- cime_config/SystemTests/pvt.py
+++ cime_config/SystemTests/pvt.py
@@ -43,89 +43,63 @@ class PVT(SystemTestsCommon):
             logger.error(error_message)
             raise RuntimeError(error_message)
 
-        # Look to see if a harvest mode has been set in the namelist and save off for the transient run
-        # If there are more than one entries in the dict, i.e. multiple user_nl_clm's found, abort.
-        # If there are multiple enteries the user_nl_clm, use the last one as the dict is ordered
+    def run_phase(self):
+        # -------------------------------------------------------------------
+        # (1) Run FATES spin-up case in potential vegetation mode
+        # -------------------------------------------------------------------
+        orig_case = self._case
+        orig_casevar = self._case.get_value("CASE")
         caseroot = self._case.get_value("CASEROOT")
-        harvest_mode_dict = find_user_nl_option(caseroot, 'clm', 'fates_harvest_mode')
-        harvest_mode = None
-        if len(harvest_mode_dict) == 1:
-            hmod = harvest_mode_dict[next(reversed(harvest_mode_dict))]
-            if len(hmod) != 0:
-                harvest_mode = hmod[-1]
-        elif (len(harvest_mode_dict)) > 1:
-            raise RuntimeError('Multiple user_nl_clm files found.  PVT assumes only a single user_nl_clm case')
+
+        # clone the main case to create spinup case
+        logger.info("PVT log:  cloning setup")
+        clone_path = "{}.potveg".format(caseroot)
+        if os.path.exists(clone_path):
+            shutil.rmtree(clone_path)
+        logger.info("PVT log:  cloning")
+        clone = self._case.create_clone(clone_path, keepexe=True)
+        logger.info("PVT log: cloning complete")
+
+        # setup the clone case
+        os.chdir(clone_path)
+        self._set_active_case(clone)
+
+        # set the clone case values
+        stop_n_pveg = 1
+        with clone:
+            # clone.set_value("CLM_ACCELERATED_SPINUP", "on")
+            clone.set_value("STOP_N", stop_n_pveg)
 
         # Modify the spin up case to use the potential vegetation mode.
         # Checks for incompatible cases and necessary mapping files are
         # handled in the build case.
-        # use_fates_lupft should be set to true in the testmod
-        # If the testmod has a fates harvest mode, make sure to append 'no_harvest'
-        # to the spin up case
+        # Turn off fates_harvest_mode for the spin up.
 
         logger.info("PVT log:  modify user_nl_clm file for spin up run")
-        added_content = ["use_fates_potentialveg = .true."]
-        if harvest_mode is not None:
-            added_content.append("fates_harvest_mode = 'no_harvest'")
-        append_to_user_nl_files(caseroot, "clm", added_content)
+        added_content = ["use_fates_potentialveg = .true.","fates_harvest_mode = 'no_harvest'"]
+        append_to_user_nl_files(clone_path, "clm", added_content)
 
-#    def run_phase(self):
-#        # -------------------------------------------------------------------
-#        # (1) Run FATES spin-up case in potential vegetation mode
-#        # -------------------------------------------------------------------
-#        orig_case = self._case
-#        orig_casevar = self._case.get_value("CASE")
-#        caseroot = self._case.get_value("CASEROOT")
-#
-#        # clone the main case to create spinup case
-#        logger.info("PVT log:  cloning setup")
-#        clone_path = "{}.potveg".format(caseroot)
-#        if os.path.exists(clone_path):
-#            shutil.rmtree(clone_path)
-#        logger.info("PVT log:  cloning")
-#        clone = self._case.create_clone(clone_path, keepexe=True)
-#        logger.info("PVT log: cloning complete")
-#
-#        # setup the clone case
-#        os.chdir(clone_path)
-#        self._set_active_case(clone)
-#        self._setup_all()
-#
-#
-#        stop_n_pveg = 1
-#        with clone:
-#            # clone.set_value("CLM_ACCELERATED_SPINUP", "on")
-#            clone.set_value("STOP_N", stop_n_pveg)
-#
-#        # Run the spin up case
-#        # As per SSP test:
-#        # "No history files expected, set suffix=None to avoid compare error"
-#        logger.info("PVT log:  starting spin-up run")
-#        dout_sr = clone.get_value("DOUT_S_ROOT")
-#        self._skip_pnl = False
-#        self.run_indv(suffix=None, st_archive=True)
-#
-#        # -------------------------------------------------------------------
-#        # (2) Run FATES transient case using restart file from spin-up
-#        # -------------------------------------------------------------------
-#        os.chdir(caseroot)
-#        self._set_active_case(orig_case)
-#
-#        # logger.info("PVT log:  modify user_nl_clm file for transient run")
-#        # self._append_to_user_nl_clm(
-#        #     [
-#        #         "use_fates_potentialveg = .true.",
-#        #     ]
-#        # )
-#
-#
-#        self._case.set_value("CLM_ACCELERATED_SPINUP", "off")
-#        self._case.set_value("RUN_TYPE", "hybrid")
-#        self._case.set_value("GET_REFCASE", False)
-#        self._case.set_value("RUN_REFCASE", "{}.potveg".format(orig_casevar))
-#        self._case.set_value("RUN_REFDATE", "1700-01-01")
-#        self._case.set_value("DOUT_S", False)
-#        self._case.flush()
-#
-#        # do the restart run (short term archiving is off)
-#        self.run_indv()
+        # Run the spin up case
+        # As per SSP test:
+        # "No history files expected, set suffix=None to avoid compare error"
+        logger.info("PVT log:  starting spin-up run")
+        dout_sr = clone.get_value("DOUT_S_ROOT")
+        self._skip_pnl = False
+        self.run_indv(suffix=None, st_archive=True)
+
+        # -------------------------------------------------------------------
+        # (2) Run FATES transient case using restart file from spin-up
+        # -------------------------------------------------------------------
+        os.chdir(caseroot)
+        self._set_active_case(orig_case)
+
+        self._case.set_value("CLM_ACCELERATED_SPINUP", "off")
+        self._case.set_value("RUN_TYPE", "hybrid")
+        self._case.set_value("GET_REFCASE", False)
+        self._case.set_value("RUN_REFCASE", "{}.potveg".format(orig_casevar))
+        self._case.set_value("RUN_REFDATE", "1700-01-01")
+        self._case.set_value("DOUT_S", False)
+        self._case.flush()
+
+        # do the restart run (short term archiving is off)
+        self.run_indv()
