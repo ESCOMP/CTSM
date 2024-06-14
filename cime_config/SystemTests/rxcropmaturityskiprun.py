6f59eab9d04c93b0e67d5b2837ca3b71fd148ba3
Sam Rabin <sam.rabin@gmail.com>
2024-06-14 15:45:26 -0600

    Remove RXCROPMATURITY SKIPRUN and SKIPBUILD tests.

--- cime_config/SystemTests/rxcropmaturityskiprun.py
+++
@@ -1,9 +0,0 @@
-from rxcropmaturityshared import RXCROPMATURITYSHARED
-
-
-class RXCROPMATURITYSKIPRUN(RXCROPMATURITYSHARED):
-    def build_indv(self, sharedlib_only=False, model_only=False):
-        self._case.set_value("BUILD_COMPLETE", "TRUE")
-
-    def run_phase(self):
-        self._run_phase(skip_run=True)
