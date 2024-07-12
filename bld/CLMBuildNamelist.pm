9ee75610973c32376f6dc407c99a0bc8d55cad83
Gregory Lemieux <glemieux@users.noreply.github.com>
2024-07-12 14:09:26 -0700

    fix how harvest mode is passed to add_default for use_fates_luh

--- bld/CLMBuildNamelist.pm
+++ bld/CLMBuildNamelist.pm
@@ -4522,7 +4522,7 @@ sub setup_logic_fates {
         add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'use_fates_luh', 'use_fates'=>$nl_flags->{'use_fates'},
                     'use_fates_lupft'=>$nl->get_value('use_fates_lupft'),
                     'use_fates_potentialveg'=>$nl->get_value('use_fates_potentialveg'),
-                    'fates_harvest_mode'=>$nl->get_value('fates_harvest_mode') );
+                    'fates_harvest_mode'=>remove_leading_and_trailing_quotes($nl->get_value('fates_harvest_mode')) );
         add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'use_fates_nocomp', 'use_fates'=>$nl_flags->{'use_fates'},
                     'use_fates_lupft'=>$nl->get_value('use_fates_lupft'),
                     'use_fates_sp'=>$nl_flags->{'use_fates_sp'} );
