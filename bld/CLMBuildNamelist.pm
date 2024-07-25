0d4a300112f16ee35332af5e72e8016ea1990c8a
Sam Rabin <sam.rabin@gmail.com>
2024-07-25 14:21:40 -0600

    Fix setting of stream_fldFileName_gdd20_baseline.

--- bld/CLMBuildNamelist.pm
+++ bld/CLMBuildNamelist.pm
@@ -4247,7 +4247,7 @@ sub setup_logic_cropcal_streams {
     add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_fldFileName_swindow_end');
     add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_fldfilename_cultivar_gdds');
     if ( &value_is_true($cropcals_rx_adapt) ) {
-      add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_fldFileName_gdd20_baseline');
+      add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_fldFileName_gdd20_baseline', 'stream_gdd20_seasons'=>$stream_gdd20_seasons);
     }
   }
 
