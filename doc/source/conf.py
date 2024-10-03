4c2361dd28b696499a5da6835664cef3b7673c4a
Sam Rabin <samrabin@ucar.edu>
2024-10-02 22:05:00 -0600

    Resolve some sphinx warnings.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -78,7 +78,7 @@ rst_epilog = """
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -176,7 +176,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {'python': ('https://docs.python.org/', None)}
 
 numfig = True
 numfig_format = {'figure': 'Figure %s',
