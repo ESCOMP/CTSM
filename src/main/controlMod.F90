d8259890179e40a3aad29b2e100a0663fe9065cc
Gregory Lemieux <glemieux@users.noreply.github.com>
2024-12-10 16:23:31 -0800

    add FATES radiation model switch to the namelist
    This commit facilitates moving this switch from the FATES parameter file
    to the namelist

--- src/main/controlMod.F90
+++ src/main/controlMod.F90
@@ -245,6 +245,7 @@ contains
           fates_stomatal_assimilation,                  &
           fates_cstarvation_model,                      &
           fates_regeneration_model,                     &
+          fates_radiation_model,                        &
           fates_hydro_solver,                           &
           fates_parteh_mode,                            &
           fates_seeddisp_cadence,                       &
@@ -808,6 +809,7 @@ contains
     call mpi_bcast (fates_stomatal_assimilation, len(fates_stomatal_assimilation) , MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (fates_cstarvation_model, len(fates_cstarvation_model) , MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (fates_regeneration_model, len(fates_regeneration_model) , MPI_CHARACTER, 0, mpicom, ier)
+    call mpi_bcast (fates_radiation_model, len(fates_radiation_model) , MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (fates_hydro_solver, len(fates_hydro_solver) , MPI_CHARACTER, 0, mpicom, ier)
     call mpi_bcast (use_fates_planthydro, 1, MPI_LOGICAL, 0, mpicom, ier)
     call mpi_bcast (use_fates_tree_damage, 1, MPI_LOGICAL, 0, mpicom, ier)
@@ -1215,6 +1217,7 @@ contains
        write(iulog, *) '    fates_stomatal_assimilation = ', fates_stomatal_assimilation
        write(iulog, *) '    fates_cstarvation_model = ', fates_cstarvation_model
        write(iulog, *) '    fates_regeneration_model = ', fates_regeneration_model
+       write(iulog, *) '    fates_radiation_model = ', fates_radiation_model
        write(iulog, *) '    fates_hydro_solver = ', fates_hydro_solver
        write(iulog, *) '    fates_paramfile = ', fates_paramfile
        write(iulog, *) '    fates_parteh_mode = ', fates_parteh_mode
