708719b0edf018e1f190e5a2e869cd591e061c81
Erik Kluzek <erik@ucar.edu>
2025-01-31 02:36:59 -0700

    Add passing test that runs the standard workflow for sspmatrix

--- python/ctsm/test/test_unit_sspmatrix.py
+++ python/ctsm/test/test_unit_sspmatrix.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 """
-Unit tests for the SystemTest sspmatrix.pty
-
-You can run this by:
-    python -m unittest test_unit_sspmatrix.py
+Unit tests for the SystemTest sspmatrix.py
 """
 
 import unittest
@@ -12,6 +9,7 @@ import sys
 import tempfile
 import shutil
 import logging
+from pathlib import Path
 
 from ctsm.path_utils import add_cime_lib_to_path
 from ctsm import unit_testing
@@ -35,6 +33,51 @@ from CIME.tests.case_fake import CaseFake
 
 logger = logging.getLogger(__name__)
 
+class SSPCaseFake(CaseFake):
+    """
+    Extend the CaseFake class with a couple things needed here
+    """
+    def __init__(self, case_root, tempdir, create_case_root=True):
+        """
+        Initialization handling the tempdir
+        """
+        super().__init__( case_root, create_case_root)
+        self._tempdir = tempdir
+
+    def create_clone(
+        self,
+        newcase,
+        keepexe=False,
+    ):
+        """
+        Extend to handle creation of user_nl_clm file
+        """
+        clone = super().create_clone( newcase, keepexe=keepexe)
+        os.mknod( os.path.join( newcase, "user_nl_clm") )
+        # Also make the needed case directories
+        clone.make_case_dirs(self._tempdir)
+        return clone
+
+    def make_case_dirs(self, tempdir):
+        """ 
+        Create the directories needed for the CASE
+        """
+        casename = self.get_value("CASE")
+        dout_s_root = Path( tempdir, "archive", casename )
+        dout_s_root.mkdir( parents=True )
+        self.set_value("DOUT_S_ROOT", str(dout_s_root) )
+        rest_root = Path( dout_s_root, "rest" )
+        rest_root.mkdir( )
+        rundir = Path( tempdir, casename, "run" )
+        rundir.mkdir()
+        self.set_value("RUNDIR", rundir )
+
+
+    def __str__( self ):
+        """
+        String method
+        """
+        return "caseroot=%s" % (self.get_value("CASEROOT"))
 
 class TestSSPMatrix(unittest.TestCase):
     """
@@ -48,25 +91,26 @@ class TestSSPMatrix(unittest.TestCase):
         self._previous_dir = os.getcwd()
         self._tempdir = tempfile.mkdtemp()
 
-        # Set up the testing CaseFake
-        caseroot = os.path.join(self._tempdir, "case")
-        self._case = CaseFake(caseroot, create_case_root=True)
+        # Set up the testing SSPCaseFake
+        caseroot = os.path.join(self._tempdir, "ssptestcase")
+        self._case = SSPCaseFake(caseroot, tempdir=self._tempdir, create_case_root=True)
         os.chdir(caseroot)
 
         # Set XML variables that will be needed in the case
         self._case.set_value("DATM_YR_START", 2000)
         self._case.set_value("DATM_YR_END", 2001)
         self._case.set_value("COMP_LND", "clm")
-        self._case.set_value("NINST", "1")
+        self._case.set_value("NINST", 1)
 
         self.ssp = SSPMATRIXCN(self._case)
+        self._case.make_case_dirs(self._tempdir)
 
     def tearDown(self):
         """
         Remove temporary directory
         """
         os.chdir(self._previous_dir)
-        shutil.rmtree(self._tempdir, ignore_errors=True)
+        #shutil.rmtree(self._tempdir, ignore_errors=True)
 
     def test_logger(self):
         """
@@ -129,6 +173,11 @@ class TestSSPMatrix(unittest.TestCase):
         self.assertEqual(expect, log, "Append user_nl_clm file NOT as expected for step 2")
         os.remove(ufile)
 
+    def test_run_phase(self):
+        """
+        Test doing the standard run_phase, that does each step
+        """
+        self.ssp.run_phase()
 
 if __name__ == "__main__":
     unit_testing.setup_for_tests()
