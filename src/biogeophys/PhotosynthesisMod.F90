df30eedb06455f760ddf47c9a3b9fe51a4844068
Keith Oleson <oleson@ucar.edu>
2025-03-18 14:09:49 -0600

    Calculate gs_mol_sun (GSSUN) and gs_mol_sha (GSSHA) at night in PhotosynthesisHydraulicStress

--- src/biogeophys/PhotosynthesisMod.F90
+++ src/biogeophys/PhotosynthesisMod.F90
@@ -3524,6 +3524,8 @@ contains
                rs_z_sha(p,iv) = min(rsmax0, 1._r8/(max( bsha(p)*gsminsha, 1._r8 )) * cf)
                ci_z_sha(p,iv) = 0._r8
                rh_leaf_sha(p) = 0._r8
+               gs_mol_sun(p,iv) = cf/rs_z_sun(p,iv)
+               gs_mol_sha(p,iv) = cf/rs_z_sha(p,iv)
 
             else                                     ! day time
 
