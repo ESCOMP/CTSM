b357945cd2815f0243cab7db14da9373e86c40ac
Sam Rabin <samrabin@ucar.edu>
2025-05-22 22:01:08 -0600

    Move some things from conf.py to substitutions.py.

--- doc/source/substitutions.py
+++ doc/source/substitutions.py
@@ -8,11 +8,56 @@ Substitutions for Sphinx
 
 # pylint: disable=invalid-name
 
-# (Standard Sphinx variable) The short X.Y version.
+#################################
+### Standard Sphinx variables ###
+#################################
+
+# General information about the project.
+project = "ctsm"
+copyright = "2020, UCAR"  # pylint: disable=redefined-builtin
+author = ""
+
+# The short X.Y version.
 version = "CLM5.0"
 
-# (Standard Sphinx variable) The full version, including alpha/beta/rc tags.
+# The full version, including alpha/beta/rc tags.
 release = "release-clm5.0"
 
-# (CTSM-specific) Version label used at the top of some pages.
+#####################################################
+### Custom variables needed for doc-builder setup ###
+#####################################################
+
+# Version label used at the top of some pages.
 version_label = "CLM5.0 (CESM2.1)"
+
+#######################################################
+### Custom variables optional for doc-builder setup ###
+#######################################################
+
+tex_category = "Miscellaneous"
+
+# Used by HTML help builder
+htmlhelp = {
+    "basename": "clmdocdoc", # Output file base name
+}
+
+# Used for LaTeX output
+latex = {
+    "target_name": "clmdoc.tex",
+    "title": "CLM Documentation",
+    "documentclass": "manual", # howto, manual, or own class
+    "category": tex_category,
+}
+
+# Used for man_pages and texinfo_documents
+mantex = {
+    "name": "clmdoc",
+    "title": "clmdoc Documentation",
+}
+
+# Used for texinfo_documents
+tex = {
+    "dirmenu_entry": "clmdoc",
+    "description": "One line description of project.",
+    "category": tex_category,
+}
