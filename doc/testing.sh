ef41f6846f8a706ee41b1d7fdd39504285f3b445
Sam Rabin <samrabin@ucar.edu>
2025-05-23 02:59:29 -0600

    Move testing.sh to its own workflow.

--- doc/testing.sh
+++ doc/testing.sh
@@ -5,6 +5,7 @@ set -x
 rm -rf _publish*
 
 # Build all docs using container
+echo "~~~~~ Build all docs using container"
 # Also do a custom --conf-py-path
 rm -rf _build _publish
 d1="$PWD/_publish_container"
@@ -13,6 +14,7 @@ d1="$PWD/_publish_container"
 cp -a _publish "${d1}"
 
 # Build all docs using ctsm_pylib
+echo "~~~~~ Build all docs using ctsm_pylib"
 rm -rf _build _publish
 d2="$PWD/_publish_nocontainer"
 conda run -n ctsm_pylib ./build_docs_to_publish -r _build --site-root "$PWD/_publish"
@@ -20,22 +22,27 @@ conda run -n ctsm_pylib ./build_docs_to_publish -r _build --site-root "$PWD/_pub
 cp -a _publish "${d2}"
 
 # Make sure container version is identical to no-container version
+echo "~~~~~ Make sure container version is identical to no-container version"
 diff -qr "${d1}" "${d2}"
 
 # Check that -r -v works
+echo "~~~~~ Check that -r -v works"
 # Also do a custom --conf-py-path
 rm -rf _build_docker
 ./build_docs -r _build_docker -v latest -d -c --conf-py-path doc-builder/test/conf.py --static-path ../_static --templates-path ../_templates
 
 # Check that Makefile method works
+echo "~~~~~ Check that Makefile method works"
 rm -rf _build
 make SPHINXOPTS="-W --keep-going" BUILDDIR=${PWD}/_build html
 
 # Check that -b works
+echo "~~~~~ Check that -b works"
 rm -rf _build_docker
 ./build_docs -b _build_docker -d -c
 
 # Check that doc-builder tests pass
+echo "~~~~~ Check that doc-builder tests pass"
 cd doc-builder/test
 conda run -n ctsm_pylib make test
 
