eebb41cc1373cb3326ecaaf1ac24ee042ef5b8f3
Sam Rabin <samrabin@ucar.edu>
2025-05-23 14:39:45 -0600

    version_list.py: Update release-clm5.0 ref.

--- doc/version_list.py
+++ doc/version_list.py
@@ -25,7 +25,7 @@ VERSION_LIST = [
     DocsVersion(
         short_name="release-clm5.0",
         display_name="CLM5.0",
-        ref="release-clm5.0-update-20250523",
+        ref="release-clm5.0",
     ),
 ]
 # End version definitions (keep this comment; Sphinx is looking for it)
