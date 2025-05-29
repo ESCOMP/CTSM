634c7a04232073737c1b3188d6e2c05f83e804e1
Sam Rabin <samrabin@ucar.edu>
2025-05-28 23:07:19 -0600

    Point doc-builder at v2.2.6...
    Prefer Podman on Mac, Docker Engine elsewhere.
    - Use --mount instead of -v where possible (on Mac, or Ubuntu Docker).
    - Don't allow build_docs_to_publish with Podman except on Mac. See ESMCI/doc-builder#27.

--- doc/testing.sh
+++ doc/testing.sh
@@ -9,7 +9,7 @@ echo "~~~~~ Build all docs using container"
 # Also do a custom --conf-py-path
 rm -rf _build _publish
 d1="$PWD/_publish_container"
-./build_docs_to_publish -r _build -d --site-root "$PWD/_publish" ${containercli}
+./build_docs_to_publish -r _build -d --site-root "$PWD/_publish"
 # VERSION LINKS WILL NOT RESOLVE IN _publish_container
 cp -a _publish "${d1}"
 
@@ -26,10 +26,10 @@ echo "~~~~~ Make sure container version is identical to no-container version"
 diff -qr "${d1}" "${d2}"
 
 # Check that -r -v works
-echo "~~~~~ Check that -r -v works"
+echo "~~~~~ Check that -r -v works (Docker)"
 # Also do a custom --conf-py-path
 rm -rf _build_container
-./build_docs -r _build_container -v latest -d ${containercli} -c --conf-py-path doc-builder/test/conf.py --static-path ../_static --templates-path ../_templates
+./build_docs -r _build_container -v latest -d -c --conf-py-path doc-builder/test/conf.py --static-path ../_static --templates-path ../_templates --container-cli-tool docker
 
 # Check that Makefile method works
 echo "~~~~~ Check that Makefile method works"
@@ -37,9 +37,9 @@ rm -rf _build
 make SPHINXOPTS="-W --keep-going" BUILDDIR=${PWD}/_build html
 
 # Check that -b works
-echo "~~~~~ Check that -b works"
+echo "~~~~~ Check that -b works (Podman)"
 rm -rf _build_container
-./build_docs -b _build_container -d ${containercli} -c
+./build_docs -b _build_container -d -c --container-cli-tool docker
 
 # Check that doc-builder tests pass
 # Don't run if on a GitHub runner; failing 🤷. Trust that doc-builder does this test.
