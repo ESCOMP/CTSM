db6a25b51f4948f87b10e92da52f80594c26c8c4
Sam Rabin <sam.rabin@gmail.com>
2025-05-30 14:01:51 -0600

    Satisfy Pylint.

--- .github/workflows/validate_xml.py
+++ .github/workflows/validate_xml.py
@@ -1,11 +1,17 @@
+"""
+Check that all XML files in our repo (except for those in submodules) are well-formed. Error if not.
+"""
 import sys
-from lxml import etree
 import glob
 import os
 import subprocess
+import xml.etree.ElementTree as ET
 
 
 def get_submodule_paths():
+    """
+    Get list of submodules
+    """
     cmd = "git config --file .gitmodules --get-regexp path | awk '{ print $2 }'"
     result = subprocess.run(cmd, capture_output=True, text=True, shell=True, check=True)
     result_list = result.stdout.split("\n")[:-1]
@@ -14,19 +20,28 @@ def get_submodule_paths():
 
 
 def is_in_submodule(file_path, submodule):
+    """
+    Return True if file is in given submodule, False otherwise
+    """
     return os.path.commonpath([file_path, submodule]) == os.path.commonpath([submodule])
 
 
 def is_in_any_submodule(file_path, submodule_paths):
+    """
+    Return True if file is in any submodule, False otherwise
+    """
     file_path = os.path.abspath(file_path)
     submodule_paths = map(os.path.abspath, submodule_paths)
     return any(is_in_submodule(file_path, submodule) for submodule in submodule_paths)
 
 
 def validate_xml(file_path):
+    """
+    Return True if XML file is well-formed, False otherwise
+    """
     try:
-        etree.parse(file_path)
-    except etree.XMLSyntaxError:
+        ET.parse(file_path)
+    except ET.ParseError:
         print(f"❌ {file_path} is NOT well-formed")
         return False
     print(f"✅ {file_path} is well-formed")
@@ -34,6 +49,7 @@ def validate_xml(file_path):
 
 
 def main():
+    # pylint: disable=missing-function-docstring
     submodule_paths = get_submodule_paths()
     all_valid = True
     for xml_file in glob.glob("**/*.xml", recursive=True):
