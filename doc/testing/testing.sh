244da8330f643c9363cbcdd55a506e8f7158592e
Sam Rabin <samrabin@ucar.edu>
2025-06-05 15:17:24 -0600

    Break up testing.sh into subscripts.

--- doc/testing/testing.sh
+++ doc/testing/testing.sh
@@ -1,55 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
-set -x
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "${SCRIPT_DIR}"
+cd "${SCRIPT_DIR}/"
 
-rm -rf _publish*
+# Compare docs built with container vs. ctsm_pylib
+./test_container_eq_ctsm_pylib.sh
 
-# Build all docs using container
-echo "~~~~~ Build all docs using container"
-# Also do a custom --conf-py-path
-rm -rf _build _publish
-d1="$PWD/_publish_container"
-./build_docs_to_publish -r _build -d --site-root "$PWD/_publish"
-# VERSION LINKS WILL NOT RESOLVE IN _publish_container
-cp -a _publish "${d1}"
-
-# Build all docs using ctsm_pylib
-echo "~~~~~ Build all docs using ctsm_pylib"
-rm -rf _build _publish
-d2="$PWD/_publish_nocontainer"
-conda run -n ctsm_pylib ./build_docs_to_publish -r _build --site-root "$PWD/_publish"  --conf-py-path doc-builder/test/conf.py --static-path ../_static --templates-path ../_templates
-# VERSION LINKS WILL NOT RESOLVE IN _publish_nocontainer
-cp -a _publish "${d2}"
-
-# Make sure container version is identical to no-container version
-echo "~~~~~ Make sure container version is identical to no-container version"
-diff -qr "${d1}" "${d2}"
-
-# Check that -r -v works
-echo "~~~~~ Check that -r -v works (Docker)"
-# Also do a custom --conf-py-path
-rm -rf _build_container
-./build_docs -r _build_container -v latest -d -c --conf-py-path doc-builder/test/conf.py --static-path ../_static --templates-path ../_templates --container-cli-tool docker
-
-# Check that Makefile method works
-echo "~~~~~ Check that Makefile method works"
+# ✅ Check that -r -v works (Docker)
+# Also do a custom --conf-py-path and other stuff
 rm -rf _build
-make SPHINXOPTS="-W --keep-going" BUILDDIR=${PWD}/_build html
+./test_build_docs_-r-v.sh docker
 
-# Check that -b works
-echo "~~~~~ Check that -b works (Podman)"
-rm -rf _build_container
-./build_docs -b _build_container -d -c --container-cli-tool docker
+# ✅ Check that Makefile method works
+rm -rf _build
+conda run --no-capture-output -n ctsm_pylib ./test_makefile_method.sh
 
-# Check that doc-builder tests pass
+# ✅ Check that -b works
+rm -rf _build
+./test_build_docs_-b.sh docker
+
+# ✅ Check that doc-builder tests pass
 # Don't run if on a GitHub runner; failing 🤷. Trust that doc-builder does this test.
 if [[ "${GITHUB_ACTIONS}" == "" ]]; then
-    echo "~~~~~ Check that doc-builder tests pass"
-    cd doc-builder/test
-    conda run -n ctsm_pylib make test
+    ./test_doc-builder_tests.sh
 fi
 
 exit 0
