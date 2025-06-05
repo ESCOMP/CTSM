245319965f54e3a94fe651c467bcd83393148c52
Sam Rabin <samrabin@ucar.edu>
2025-06-05 16:13:37 -0600

    Trying to fix docs-omnibus workflow.

--- doc/testing/testing.sh
+++ doc/testing/testing.sh
@@ -9,20 +9,24 @@ cd "${SCRIPT_DIR}/"
 
 # Check that -r -v works (Docker)
 # Also do a custom --conf-py-path and other stuff
+cd "${SCRIPT_DIR}/"
 rm -rf _build
 ./test_build_docs_-r-v.sh docker
 
 # Check that Makefile method works
+cd "${SCRIPT_DIR}/"
 rm -rf _build
 conda run --no-capture-output -n ctsm_pylib ./test_makefile_method.sh
 
 # Check that -b works
+cd "${SCRIPT_DIR}/"
 rm -rf _build
 ./test_build_docs_-b.sh docker
 
 # Check that doc-builder tests pass
 # Don't run if on a GitHub runner; failing 🤷. Trust that doc-builder does this test.
 if [[ "${GITHUB_ACTIONS}" == "" ]]; then
+    cd "${SCRIPT_DIR}/"
     ./test_doc-builder_tests.sh
 fi
 
