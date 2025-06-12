d4f7f607644642af1ed55cfa5a87bedef8e78258
Sam Rabin <sam.rabin@gmail.com>
2025-06-12 15:38:20 -0600

    Add FUNITFATES SystemTest.

---
+++ cime_config/SystemTests/funitfates.py
@@ -0,0 +1,40 @@
+"""
+Implementation of the FATES unit tests.
+
+This "system" test runs FATES's Fortran unit tests. We're abusing the system test
+infrastructure to run these, so that a run of the test suite can result in the unit tests
+being run as well.
+
+Grid and compset are irrelevant for this test type.
+"""
+
+import os
+import sys
+from CIME.SystemTests.funit import FUNIT
+from CIME.XML.standard_module_setup import *
+import systemtest_utils as stu
+
+_FATES_TESTING_PYTHON = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), os.pardir, os.pardir, "src", "fates", "testing"
+)
+
+logger = logging.getLogger(__name__)
+
+
+class FUNITFATES(FUNIT):
+    def __init__(self, case):
+        FUNIT.__init__(self, case)
+
+    def run_phase(self):
+        # Change to the FATES testing directory
+        os.chdir(_FATES_TESTING_PYTHON)
+
+        # Run the testing
+        script_name = "run_unit_tests.py"
+
+        stu.run_python_script(
+            self._get_caseroot(),
+            "ctsm_pylib",
+            os.path.join(os.curdir, script_name),
+            os.path.join(os.curdir, script_name),
+        )
