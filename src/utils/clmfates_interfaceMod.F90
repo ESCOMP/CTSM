fb548696f5977571764c72e921ec1499d4c6c011
Gregory Lemieux <glemieux@users.noreply.github.com>
2025-08-04 11:01:29 -0700

    add call to pass managed fire to fates

--- src/utils/clmfates_interfaceMod.F90
+++ src/utils/clmfates_interfaceMod.F90
@@ -504,6 +504,7 @@ module CLMFatesInterfaceMod
         else
            pass_managed_fire = 0
         end if
+        call set_fates_ctrlparms('managed_fire',ival=pass_managed_fire)
 
 
         ! This has no variable on the FATES side yet (RGK)
