0e2e1e3c9a63af9948511fcee58dba76da2376e4
Sam Rabin <sam.rabin@gmail.com>
2025-08-07 17:10:06 -0600

    query_paramfile: Accept comma-separated variable list.

--- python/ctsm/query_parameters/query_paramfile.py
+++ python/ctsm/query_parameters/query_paramfile.py
@@ -3,19 +3,33 @@ import xarray as xr
 
 
 def get_arguments():
-    parser = argparse.ArgumentParser(description="Print values of a variable from a netCDF file.")
+    parser = argparse.ArgumentParser(
+        description="Print values of one or more variables from a netCDF file."
+    )
     parser.add_argument("-i", "--input", required=True, help="Input netCDF file")
-    parser.add_argument("variable", help="Name of variable to extract")
+    parser.add_argument("variables", help="Comma-separated list of variable names to extract")
     args = parser.parse_args()
     return args
+
+
+def print_values(ds, var):
+    data = ds[var].values
+    print(f"{var}: {data}")
 
 
 def main():
     args = get_arguments()
 
+    variable_names = [v.strip() for v in args.variables.split(",")]
+
     ds = xr.open_dataset(args.input, decode_timedelta=False)
-    data = ds[args.variable].values
-    print(f"{args.variable}: {data}")
+
+    for var in variable_names:
+        if var in ds.variables:
+            print_values(ds, var)
+        else:
+            print(f"Variable '{var}' not found in {args.input}")
+
     ds.close()
 
 
