042f5f4c65c90d8a69bfedcd96b6327d53296847
Sam Rabin <sam.rabin@gmail.com>
2025-08-15 13:12:38 -0600

    netcdf_utils: Add are_xr_dataarrays_identical().

--- python/ctsm/netcdf_utils.py
+++ python/ctsm/netcdf_utils.py
@@ -2,6 +2,8 @@
 Helper functions for working with netCDF files
 """
 
+import numpy as np
+import xarray as xr
 from netCDF4 import Dataset  # pylint: disable=no-name-in-module
 
 
@@ -12,3 +14,62 @@ def get_netcdf_format(file_path):
     with Dataset(file_path, "r") as netcdf_file:
         netcdf_format = netcdf_file.data_model
     return netcdf_format
+
+
+def are_xr_dataarrays_identical(da0: xr.DataArray, da1: xr.DataArray):
+    """
+    Comprehensively check whether two DataArrays are identical
+    """
+    # pylint: disable=too-many-return-statements
+
+    # Check data type
+    if da0.dtype != da1.dtype:
+        return False
+
+    # Check encoding
+    if da0.encoding != da1.encoding:
+        return False
+
+    # Check attributes
+    if da0.attrs != da1.attrs:
+        return False
+
+    # Check name
+    if da0.name != da1.name:
+        return False
+
+    # Check dims
+    if da0.dims != da1.dims:
+        return False
+
+    # Check sizes
+    if da0.sizes != da1.sizes:
+        return False
+
+    # Check coordinates
+    if bool(da0.coords) or bool(da1.coords):
+        if not bool(da0.coords) or not bool(da1.coords):
+            return False
+        if not da0.coords.equals(da1.coords):
+            return False
+
+    # Check values ("The array's data converted to numpy.ndarray")
+    if not np.array_equal(da0.values, da1.values):
+        # Try-except to avoid TypeError from putting NaN-incapable dtypes through
+        # np.array_equal(..., equal_nan=True)
+        try:
+            if not np.array_equal(da0.values, da1.values, equal_nan=True):
+                return False
+        except TypeError:
+            return False
+
+    # Check data ("The DataArray's data as an array. The underlying array type (e.g. dask, sparse,
+    # pint) is preserved.")
+    da0_data_type = type(da0.data)
+    if not isinstance(da1.data, da0_data_type):
+        return False
+    if not isinstance(da0.data, np.ndarray):
+        raise NotImplementedError(f"Add support for comparing two objects of type {da0_data_type}")
+
+    # Fallback to however xarray defines equality, in case we missed something above
+    return da0.equals(da1)
--- python/ctsm/test/test_unit_netcdf_utils.py
+++ python/ctsm/test/test_unit_netcdf_utils.py
@@ -8,6 +8,8 @@ import sys
 import unittest
 import shutil
 import tempfile
+import numpy as np
+import pandas as pd
 import xarray as xr
 
 # -- add python/ctsm  to path (needed if we want to run the test stand-alone)
@@ -15,7 +17,7 @@ _CTSM_PYTHON = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pard
 sys.path.insert(1, _CTSM_PYTHON)
 
 # pylint: disable=wrong-import-position
-from ctsm.netcdf_utils import get_netcdf_format
+import ctsm.netcdf_utils as nu
 from ctsm import unit_testing
 
 # pylint: disable=invalid-name
@@ -41,7 +43,7 @@ class TestUnitGetNetcdfFormat(unittest.TestCase):
         """
         nc_format = "NETCDF3_CLASSIC"
         self.ds.to_netcdf(self.outfile, format=nc_format)
-        self.assertEqual(get_netcdf_format(self.outfile), nc_format)
+        self.assertEqual(nu.get_netcdf_format(self.outfile), nc_format)
 
     def test_get_netcdf_format_netcdf4(self):
         """
@@ -49,7 +51,214 @@ class TestUnitGetNetcdfFormat(unittest.TestCase):
         """
         nc_format = "NETCDF4"
         self.ds.to_netcdf(self.outfile, format=nc_format)
-        self.assertEqual(get_netcdf_format(self.outfile), nc_format)
+        self.assertEqual(nu.get_netcdf_format(self.outfile), nc_format)
+
+
+class TestUnitAreXrDataArraysIdentical(unittest.TestCase):
+    """
+    Unit tests for are_xr_dataarrays_identical
+    """
+
+    def test_are_xr_dataarrays_identical_data_types_match(self):
+        """Should be true if dtypes match"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_data_types_differ(self):
+        """Should be false if dtypes differ"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(np.float64(1))
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_data_types_differ_precision(self):
+        """Should be false if dtypes differ only in their precision"""
+        da0 = xr.DataArray(np.float64(1))
+        da1 = xr.DataArray(np.float32(1))
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_encodings_match(self):
+        """Should be true if encodings are the exact same"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.encoding["dummy0"] = -999
+        da1.encoding["dummy0"] = -999
+        da0.encoding["dummy1"] = -999
+        da1.encoding["dummy1"] = -999
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_encodings_match_except_order(self):
+        """Should be true if encodings are the same as long as you don't care about order"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.encoding["dummy0"] = -999
+        da0.encoding["dummy1"] = -999
+        da1.encoding["dummy1"] = -999
+        da1.encoding["dummy0"] = -999
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_encodings_differ_number(self):
+        """Should be false if encodings have a different number of keys"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.encoding["dummy0"] = -999
+        da1.encoding["dummy0"] = -999
+        da1.encoding["dummy1"] = -999
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_encodings_differ(self):
+        """Should be false if encodings have the same number and order of keys but not values"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.encoding["dummy0"] = -999
+        da1.encoding["dummy0"] = -999
+        da0.encoding["dummy1"] = -998
+        da1.encoding["dummy1"] = -999
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_attributes_match(self):
+        """Should be true if attributes are the exact same"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.attrs["dummy0"] = -999
+        da1.attrs["dummy0"] = -999
+        da0.attrs["dummy1"] = -999
+        da1.attrs["dummy1"] = -999
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_attributes_match_nan(self):
+        """Should be true if attributes are the exact same and NaN"""
+        da0 = xr.DataArray(1.0)
+        da1 = xr.DataArray(1.0)
+        da0.attrs["_FillValue"] = np.float64(np.nan)
+        da1.attrs["_FillValue"] = np.float64(np.nan)
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_attributes_match_except_order(self):
+        """Should be true if attributes are the same as long as you don't care about order"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.attrs["dummy0"] = -999
+        da0.attrs["dummy1"] = -999
+        da1.attrs["dummy1"] = -999
+        da1.attrs["dummy0"] = -999
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_attributes_differ_number(self):
+        """Should be false if attributes have a different number of keys"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.attrs["dummy0"] = -999
+        da1.attrs["dummy0"] = -999
+        da1.attrs["dummy1"] = -999
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_attributes_differ(self):
+        """Should be false if attributes have the same number and order of keys but not values"""
+        da0 = xr.DataArray(int(1))
+        da1 = xr.DataArray(int(1))
+        da0.attrs["dummy0"] = -999
+        da1.attrs["dummy0"] = -999
+        da0.attrs["dummy1"] = -998
+        da1.attrs["dummy1"] = -999
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_values_ndarrays_match(self):
+        """Should be true if values match and they're both numpy arrays under the hood"""
+        da0 = xr.DataArray(np.array(int(1)))
+        da1 = xr.DataArray(np.array(int(1)))
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_values_ndarrays_differ(self):
+        """Should be false if values differ and they're both numpy arrays under the hood"""
+        da0 = xr.DataArray(np.array(int(1)))
+        da1 = xr.DataArray(np.array(int(2)))
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_coords_match(self):
+        """Should be true if coordinates match"""
+        time = pd.date_range("2000-01-01", periods=3)
+        da0 = xr.DataArray(
+            dims=["time"],
+            coords={"time": time},
+        )
+        da1 = xr.DataArray(
+            dims=["time"],
+            coords={"time": time},
+        )
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+        # To make sure we don't need a separate "test_are_xr_dataarrays_identical_indexes_match"
+        self.assertEqual(len(da0.indexes), len(da1.indexes))
+        for key in da0.indexes:
+            self.assertTrue(da0.indexes[key].equals(da1.indexes.get(key)))
+
+    def test_are_xr_dataarrays_identical_coords_onemissing(self):
+        """Should be false if only one has coords"""
+        data = [1, 2, 3]
+        time = pd.date_range("2000-01-01", periods=len(data))
+        da0 = xr.DataArray(
+            data=data,
+            dims=["time"],
+            coords={"time": time},
+        )
+        da1 = xr.DataArray(
+            data=data,
+            dims=["time"],
+        )
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_coords_differ(self):
+        """Should be false if coordinates differ"""
+        time0 = pd.date_range("2000-01-01", periods=3)
+        da0 = xr.DataArray(
+            dims=["time"],
+            coords={"time": time0},
+        )
+        time1 = pd.date_range("1987-01-01", periods=3)
+        da1 = xr.DataArray(
+            dims=["time"],
+            coords={"time": time1},
+        )
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_dims_match(self):
+        """Should be true if dimensions match"""
+        da0 = xr.DataArray(data=[1], dims=["dim"])
+        da1 = xr.DataArray(data=[1], dims=["dim"])
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_dims_differ(self):
+        """Should be false if dimensions differ"""
+        da0 = xr.DataArray(data=[1], dims=["dim0"])
+        da1 = xr.DataArray(data=[1], dims=["dim1"])
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_names_match(self):
+        """Should be true if names match"""
+        da0 = xr.DataArray(data=[1], name="da_name")
+        da1 = xr.DataArray(data=[1], name="da_name")
+        self.assertTrue(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_names_differ(self):
+        """Should be false if names differ"""
+        da0 = xr.DataArray(data=[1], name="da0_name")
+        da1 = xr.DataArray(data=[1], name="da1_name")
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    def test_are_xr_dataarrays_identical_sizes_differ(self):
+        """Should be false if sizes differ"""
+        da0 = xr.DataArray(data=[1])
+        da1 = xr.DataArray(data=[1, 1])
+        self.assertFalse(nu.are_xr_dataarrays_identical(da0, da1))
+
+    # Waiting on dask, sparse, or pint to be in ctsm_pylib:
+    # TODO: False if data types don't match
+    # TODO: NotImplementedError if data types match but aren't np.ndarray
+
+    # Waiting on dask to be in ctsm_pylib:
+    # TODO: True if the only difference is chunked or not
+    # TODO: True if the only difference is chunk sizes
 
 
 if __name__ == "__main__":
