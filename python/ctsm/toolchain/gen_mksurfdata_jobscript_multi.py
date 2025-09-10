e69570af3c4b5bb82734c57b9e215149d86a9ee0
Samuel Levis <slevis@ucar.edu>
2025-09-10 15:44:52 -0600

    Update Makefile/_multi.py to accommodate new fsurdat/landuse needs

--- python/ctsm/toolchain/gen_mksurfdata_jobscript_multi.py
+++ python/ctsm/toolchain/gen_mksurfdata_jobscript_multi.py
@@ -32,7 +32,7 @@ valid_scenarios = [
     "crop-global-present-low-res",
     "crop-global-present-ne16",
     "crop-global-present-ne30",
-    "crop-global-present-ne120",
+    "crop-global-present-vrandne120",
     "crop-global-present-mpasa480",
     "crop-global-present-nldas",
     "crop-global-1850",
@@ -52,7 +52,7 @@ valid_scenarios = [
     "crop-global-SSP2-4.5-f19",
     "crop-global-SSP2-4.5-f10",
     "crop-global-SSP2-4.5-f45",
-    "crop-global-SSP2-4.5-ne0np4",
+    "crop-global-SSP2-4.5-vrandne120",
     "crop-global-SSP2-4.5-ne3",
     "crop-global-SSP2-4.5-ne16",
     "crop-global-SSP2-4.5-ne30",
@@ -193,7 +193,7 @@ def main():
     # ],
     # --------------------------
     resolution_dict = {
-        "standard_res_no_crop": ["0.9x1.25", "1.9x2.5", "mpasa60"],
+        "standard_res_no_crop": ["0.9x1.25", "1.9x2.5", "mpasa60", "mpasa30"],
         "f09": ["0.9x1.25"],
         "f19": ["1.9x2.5"],
         "hcru": ["360x720cru"],
@@ -212,17 +212,13 @@ def main():
         "ne3": ["ne3np4", "ne3np4.pg3"],
         "ne16": ["ne16np4.pg3"],
         "ne30": ["ne30np4.pg3", "ne30np4.pg2", "ne30np4"],
-        "ne0np4": [
+        "ne120": ["ne120np4.pg3"],
+        "vr_and_ne120": [
             "ne0np4.ARCTICGRIS.ne30x8",
             "ne0np4.ARCTIC.ne30x4",
             "ne0np4CONUS.ne30x8",
             "ne0np4.POLARCAP.ne30x4",
-        ],
-        "ne120": [
-            "ne0np4.ARCTICGRIS.ne30x8",
-            "ne0np4.ARCTIC.ne30x4",
-            "ne0np4CONUS.ne30x8",
-            "ne0np4.POLARCAP.ne30x4",
+            "ne0np4.NATL.ne30x8",
             "ne120np4.pg3",
         ],
     }
@@ -275,9 +271,9 @@ def main():
             "--start-year 2000 --end-year 2000                                 --res",
             "ne30",
         ),
-        "crop-global-present-ne120": (
+        "crop-global-present-vrandne120": (
             "--start-year 2000 --end-year 2000                                 --res",
-            "ne120",
+            "vr_and_ne120",
         ),
         "crop-global-present-mpasa480": (
             "--start-year 2000 --end-year 2000                                 --res",
@@ -359,9 +355,9 @@ def main():
             "--start-year 1850 --end-year 2100 --nosurfdata --ssp-rcp SSP2-4.5 --res",
             "f45",
         ),
-        "crop-global-SSP2-4.5-ne0np4": (
-            "--start-year 1979 --end-year 2026 --ssp-rcp SSP2-4.5 --res",
-            "ne0np4",
+        "crop-global-SSP2-4.5-vrandne120": (
+            "--start-year 1979 --end-year 2030 --ssp-rcp SSP2-4.5 --res",
+            "vr_and_ne120",
         ),
         "crop-global-SSP2-4.5-ne3": (
             "--start-year 1850 --end-year 2100 --nosurfdata --ssp-rcp SSP2-4.5 --res",
