d00588bb64c6fd4d3dac959033309f99669b3a01
Sam Rabin <sam.rabin@gmail.com>
2025-10-10 15:59:06 -0600

    Add some basic unit tests of vegtype_str2int(). One fails.

---
+++ python/ctsm/test/test_unit_cropcal_utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""Unit tests for cropcal_utils.py"""
+
+import unittest
+
+from ctsm import unit_testing
+from ctsm.crop_calendars import cropcal_utils as ccu
+
+# Allow names that pylint doesn't like, because otherwise I find it hard
+# to make readable unit test names
+# pylint: disable=invalid-name
+
+
+class TestCropCalUtils(unittest.TestCase):
+    """Tests of cropcal_utils.py"""
+
+    def setUp(self):
+        self.vegtype_mainlist = ["crop_1", "crop_2", "crop_3"]
+
+    def test_vegtype_str2int_1string(self):
+        """
+        Tests vegtype_str2int() for a single string
+        """
+        ccu.vegtype_str2int("crop_1", vegtype_mainlist=self.vegtype_mainlist)
+
+    def test_vegtype_str2int_2strings(self):
+        """
+        Tests vegtype_str2int() for two strings
+        """
+        result = ccu.vegtype_str2int(["crop_1", "crop_3"], vegtype_mainlist=self.vegtype_mainlist)
+        self.assertListEqual(result, [0, 2])
+
+
+if __name__ == "__main__":
+    unit_testing.setup_for_tests()
+    unittest.main()
