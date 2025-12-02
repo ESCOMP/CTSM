945fec556a7506fb49a62874ccd995e385cc140c
Erik Kluzek <erik@ucar.edu>
2025-12-01 22:48:58 -0700

    Remove the C13 Advance call so answers remain identical

--- src/biogeochem/CNCIsoAtmTimeSeriesReadMod.F90
+++ src/biogeochem/CNCIsoAtmTimeSeriesReadMod.F90
@@ -696,7 +696,8 @@ contains
     ! !LOCAL VARIABLES:
     integer :: g   ! Indices
 
-    call atm_c13_stream%Advance( )
+    ! TODO: Keep the Advance commented out until we can change answers
+    !call atm_c13_stream%Advance( )
     call atm_c13_stream%Interp( bounds)
 
     do g = bounds%begg, bounds%endg
