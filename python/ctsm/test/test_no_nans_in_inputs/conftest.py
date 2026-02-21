965acd751472df9cc502f09eaed49250a2036d6d
Sam Rabin <sam.rabin@gmail.com>
2026-02-21 12:42:46 -0700

    Remove unnecessary path addition in conftest.py.

--- python/ctsm/test/test_no_nans_in_inputs/conftest.py
+++ python/ctsm/test/test_no_nans_in_inputs/conftest.py
@@ -2,19 +2,8 @@
 Pytest configuration and fixtures for test_no_nans_in_inputs tests.
 """
 
-import os
-import sys
-
 import pytest
 
-# Add the python directory to sys.path so we can import ctsm modules
-_CTSM_PYTHON = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)
-)
-if _CTSM_PYTHON not in sys.path:
-    sys.path.insert(1, _CTSM_PYTHON)
-
-# pylint: disable=wrong-import-position
 from ctsm.no_nans_in_inputs import constants
 from ctsm.no_nans_in_inputs import replace_fill_values
 
