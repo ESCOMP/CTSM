6f74e408f5503717dab521f5889006ff50643ee9
Sam Rabin <sam.rabin@gmail.com>
2026-02-21 12:47:25 -0700

    Move path addition to __init__.py.

--- python/ctsm/no_nans_in_inputs/__init__.py
+++ python/ctsm/no_nans_in_inputs/__init__.py
@@ -1,3 +1,12 @@
 """
 Module for handling NaN fill values in CTSM input files.
 """
+
+import os
+import sys
+
+_CTSM_PYTHON = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+print(_CTSM_PYTHON)
+sys.path.insert(1, _CTSM_PYTHON)
+# pylint: disable=wrong-import-position
+from ctsm.no_nans_in_inputs.constants import ATTR, NEW_FILLVALUES_FILE, USER_REQ_DELETE, XML_FILE
--- python/ctsm/no_nans_in_inputs/replace_fill_values.py
+++ python/ctsm/no_nans_in_inputs/replace_fill_values.py
@@ -18,10 +18,6 @@ import xml.etree.ElementTree as ET
 
 import xarray as xr
 
-_CTSM_PYTHON = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
-print(_CTSM_PYTHON)
-sys.path.insert(1, _CTSM_PYTHON)
-# pylint: disable=wrong-import-position
 from ctsm.no_nans_in_inputs.constants import ATTR, NEW_FILLVALUES_FILE, USER_REQ_DELETE, XML_FILE
 
 
