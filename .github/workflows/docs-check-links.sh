33a052ce22f49c2594e0b68b70d5379d7e950e59
Samuel Levis <slevis@ucar.edu>
2026-03-20 11:12:41 -0600

    Add docs-check-links.yml and .sh files in .github/workflows

---
+++ .github/workflows/docs-check-links.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+# Check that documentation links work
+
+# The next line returns code 200 only if the page exists
+link_code="$(curl -s -o /dev/null -w "%{http_code}" $URL)"
+
+# Find bad links
+set +e
+good_links="200"
+if [[ "${link_code}" != "200" ]]; then
+    echo "One or more links in the documentation failed:" >&2
+    echo $link_code  >&2
+    exit 1
+fi
+
+exit 0
