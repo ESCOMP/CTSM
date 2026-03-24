08bd5ccb860068d457922d62c381df8d9c8e5954
Samuel Levis <slevis@ucar.edu>
2026-03-24 16:11:36 -0600

    Replace references to cheyenne with derecho in python/ctsm codes

--- python/ctsm/machine_defaults.py
+++ python/ctsm/machine_defaults.py
@@ -44,30 +44,6 @@ MachineDefaults = namedtuple(
 QsubDefaults = namedtuple("QsubDefaults", ["queue", "walltime", "extra_args", "required_args"])
 
 MACHINE_DEFAULTS = {
-    "cheyenne": MachineDefaults(
-        job_launcher_type=JOB_LAUNCHER_QSUB,
-        scratch_dir=os.path.join(os.path.sep, "glade", "scratch", get_user()),
-        baseline_dir=os.path.join(
-            os.path.sep, "glade", "p", "cgd", "tss", "To_Be_Safely_Deleted", "ctsm_baselines"
-        ),
-        account_required=True,
-        create_test_retry=0,
-        # NOTE(wjs, 2022-02-23) By default, use the regular queue, even for
-        # single-processor jobs. This is because the share queue has been really flaky,
-        # with lots of job failures or slow-running jobs.
-        create_test_queue="regular",
-        job_launcher_defaults={
-            JOB_LAUNCHER_QSUB: QsubDefaults(
-                queue="regular",
-                walltime="11:50:00",
-                extra_args="",
-                # The following assumes a single node, with a single mpi proc; we may want
-                # to add more flexibility in the future, making the node / proc counts
-                # individually selectable
-                required_args="-l select=1:ncpus=36:mpiprocs=1 -V -r n -l inception=login -k oed",
-            )
-        },
-    ),
     "derecho": MachineDefaults(
         job_launcher_type=JOB_LAUNCHER_QSUB,
         scratch_dir=os.path.join(os.path.sep, "glade", "derecho", "scratch", get_user()),
@@ -87,7 +63,7 @@ MACHINE_DEFAULTS = {
             )
         },
     ),
-    "hobart": MachineDefaults(
+    "thorodin": MachineDefaults(
         job_launcher_type=JOB_LAUNCHER_QSUB,
         scratch_dir=os.path.join(os.path.sep, "scratch", "cluster", get_user()),
         baseline_dir=os.path.join(os.path.sep, "fs", "cgd", "csm", "ccsm_baselines"),
