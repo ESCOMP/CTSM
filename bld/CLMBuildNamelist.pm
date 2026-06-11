3fb304677f41b90096ca43c618f92c0d29e63991
Erik Kluzek <erik@ucar.edu>
2026-06-11 13:58:19 -0600

    Whitespace formatting and remove the uneeded check for the driver, since nuopc is the only option now

--- bld/CLMBuildNamelist.pm
+++ bld/CLMBuildNamelist.pm
@@ -3514,12 +3514,10 @@ sub setup_logic_methane {
     my $finundation_method = remove_leading_and_trailing_quotes($nl->get_value('finundation_method' ));
     # prognostic inundation does not require an input stream; other methods do
     if($finundation_method ne 'h2osfc') {
-	add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_fldfilename_ch4finundated',
+	     add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_fldfilename_ch4finundated',
 		    'finundation_method'=>$finundation_method);
-	if ($opts->{'driver'} eq "nuopc" ) {
-	    add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_meshfile_ch4finundated',
+	     add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'stream_meshfile_ch4finundated',
 			'finundation_method'=>$finundation_method);
-	}
     }
     add_default($opts, $nl_flags->{'inputdata_rootdir'}, $definition, $defaults, $nl, 'use_aereoxid_prog',
                 'use_cn'=>$nl_flags->{'use_cn'}, 'use_fates'=>$nl_flags->{'use_fates'} );
