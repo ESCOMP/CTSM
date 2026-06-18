f5fa4c8780305b9bbd58e41ca201becc83712285
Sam Rabin <samrabin@ucar.edu>
2026-06-18 12:02:04 -0600

    test_container_eq_ctsm_pylib.sh: Verbose.

--- doc/test/test_container_eq_ctsm_pylib.sh
+++ doc/test/test_container_eq_ctsm_pylib.sh
@@ -17,7 +17,7 @@ echo "~~~~~ Build all docs using container"
 # Also do a custom --conf-py-path
 rm -rf _build _publish
 d1="$PWD/_publish_container"
-./build_docs_to_publish -r _build -d --site-root "$PWD/_publish"
+./build_docs_to_publish -r _build -d --site-root "$PWD/_publish" --verbose
 # VERSION LINKS WILL NOT RESOLVE IN _publish_container
 cp -a _publish "${d1}"
 
