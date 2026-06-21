c32098f491c498abd890481b9155808b7e7a5678
Sam Rabin <samrabin@ucar.edu>
2026-06-21 17:40:53 -0600

    GitHub workflows now cache/use cached ctsm_pylib.

--- doc/test/test_doc-builder_tests.sh
+++ doc/test/test_doc-builder_tests.sh
@@ -3,12 +3,14 @@
 # Fail on any non-zero exit code
 set -e
 
+cli_tool="${1:-conda}"
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${SCRIPT_DIR}"
 
 echo "~~~~~ Check that doc-builder tests pass"
 cd ../doc-builder/test
 set -x
-conda run --no-capture-output -n ctsm_pylib make test
+${cli_tool} run --no-capture-output -n ctsm_pylib make test
 
 exit 0
--- doc/test/testing.sh
+++ doc/test/testing.sh
@@ -3,11 +3,13 @@
 # Fail on any non-zero exit code
 set -e
 
+cli_tool="${1:-conda}"
+
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${SCRIPT_DIR}/"
 
 # Compare docs built with container vs. ctsm_pylib
-./test_container_eq_ctsm_pylib.sh
+./test_container_eq_ctsm_pylib.sh ${cli_tool}
 
 # Check that -r -v works (Docker)
 # Also do a custom --conf-py-path and other stuff
@@ -18,7 +20,7 @@ rm -rf _build
 # Check that Makefile method works
 cd "${SCRIPT_DIR}/"
 rm -rf _build
-conda run --no-capture-output -n ctsm_pylib ./test_makefile_method.sh
+${cli_tool} run --no-capture-output -n ctsm_pylib ./test_makefile_method.sh
 
 # Check that -b works
 cd "${SCRIPT_DIR}/"
@@ -29,7 +31,7 @@ rm -rf _build
 # Don't run if on a GitHub runner; failing 🤷. Trust that doc-builder does this test.
 if [[ "${GITHUB_ACTIONS}" == "" ]]; then
     cd "${SCRIPT_DIR}/"
-    ./test_doc-builder_tests.sh
+    ./test_doc-builder_tests.sh ${cli_tool}
 fi
 
 exit 0
