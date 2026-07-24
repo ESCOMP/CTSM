fd573cca9d3fc14c6d5bfa57c593344a081022b9
Samuel Levis <slevis@ucar.edu>
2026-07-24 16:35:08 -0700

    Update src/biogeochem/CNDriverMod.F90
    Resolving conflict 1 from merging master

--- src/biogeochem/CNDriverMod.F90
+++ src/biogeochem/CNDriverMod.F90
@@ -482,8 +482,7 @@ contains
                                      cnveg_carbonflux_inst,cnveg_nitrogenstate_inst,cnveg_nitrogenflux_inst,   &
                                      soilbiogeochem_carbonflux_inst,&
                                      soilbiogeochem_state_inst,soilbiogeochem_nitrogenstate_inst,              &
-                                     soilbiogeochem_nitrogenflux_inst,canopystate_inst, drydepvel_inst)
-                                     soilbiogeochem_nitrogenflux_inst,canopystate_inst, clm_fates)
+                                     soilbiogeochem_nitrogenflux_inst,canopystate_inst, drydepvel_inst, clm_fates)
      call t_stopf('soilbiogeochemcompetition')
 
     ! distribute the available N between the competing patches  on the basis of 
