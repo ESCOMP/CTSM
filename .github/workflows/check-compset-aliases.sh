a1cedb4c463fd451291cc8e177e7702830838bfe
Erik Kluzek <erik@ucar.edu>
2026-07-31 18:37:15 -0600

    Turn into a subroutine that can be called with different patterns and several patterns to check

--- .github/workflows/check-compset-aliases.sh
+++ .github/workflows/check-compset-aliases.sh
@@ -9,17 +9,71 @@ cd "$(git rev-parse --show-toplevel)"
 # Check that query_config can run without error
 cime/scripts/query_config --compsets 1>/dev/null
 
-# Find bad compsets
-OLD_IFS=$IFS
-IFS='\n'
-set +e
-# Relies on case sensitivity here: Alias should have Clm6 and longname should have CLM6
-bad_compsets="$(cime/scripts/query_config --compsets | sort | uniq | grep Clm6 | grep -v CLM6)"
-set -e
-if [[ "${bad_compsets}" != "" ]]; then
-    echo "One or more compsets with Clm6 alias but not CLM6 longname:" >&2
-    echo $bad_compsets  >&2
-    exit 1
-fi
+bad_compsets() {
+  # Subroutine to Find bad compsets
+  # Set input arguments
+  ALIAS=$1
+  LCOMPSET=$2
+  OLD_IFS=$IFS
+  IFS='\n'
+  set +e
+  # Relies on case sensitivity here: Alias should have $ALIAS and longname should have $LCOMPSET
+  bad_compsets="$(cime/scripts/query_config --compsets | sort | uniq | grep --fixed-strings $ALIAS | grep --fixed-strings --invert-match $LCOMPSET)"
+  set -e
+  if [[ "${bad_compsets}" != "" ]]; then
+      echo "One or more compsets with $ALIAS alias but not $LCOMPSET longname:" >&2
+      echo $bad_compsets  >&2
+      exit 1
+  fi
+}
+
+# Now call the subroutine for various options
+# -- Physics versions ---
+bad_compsets Clm60 CLM60
+bad_compsets Clm50 CLM50
+bad_compsets Ctsm50 "CLM50%NWP"
+# -- stub ROF or GLC
+bad_compsets Rs SROF
+bad_compsets Gs SGLC
+# -- General veg type --
+bad_compsets Bgc "%BGC"
+bad_compsets BgcCrop "%BGC-CROP"
+bad_compsets Clm50Sp "%SP"
+bad_compsets Clm60Sp "%SP"
+# ---- Note that specifying SP for NWP compsets as they shouldn't use BGC ----
+bad_compsets Nwp "%NWP-SP"
+# ---- NWP cases should always be stug GLC ---
+bad_compsets Nwp Gs
+bad_compsets Fates "%FATES"
+bad_compsets FatesSp "%FATES-SP"
+# ---- This fails with the error: grep: thro ": No such file or directory
+# ---- So commenting out ----
+#bad_compsets 'NoAnthro ' 'NOANTHRO_'
+
+# -- Period of simulation --
+bad_compsets I2000 2000_
+bad_compsets I2010 2010_
+bad_compsets I1850 1850_
+bad_compsets IHist HIST_
+bad_compsets ISSP585 SSP585_
+bad_compsets ISSP126 SSP126_
+bad_compsets ISSP119 SSP119_
+bad_compsets ISSP245 SSP245_
+bad_compsets ISSP370 SSP370_
+bad_compsets ISSP460 SSP460_
+bad_compsets ISSP534 SSP534_
+# --- DATM forcing types ---
+bad_compsets I1Pt "DATM%1PT"
+bad_compsets Crujra "DATM%CRUJRA2024b"
+bad_compsets Gswp "DATM%GSWP3v1"
+bad_compsets "Cru " "DATM%CRU"
+bad_compsets Nldas "DATM%NLDAS2"
+bad_compsets Qian '_DATM%QIA_'
+# ---- This fails with the error: grep: up ": No such file or directory
+# ---- So commenting out ----
+#bad_compsets 'Spinup ' 'DATM%CPLHIST'
+bad_compsets Miz _MIZUROUTE_
+bad_compsets "Rtm " _RTM_
+bad_compsets RtmFl "_RTM%FLOOD_"
 
 exit 0
