90c332134cea1698b10002544db22229a0989cee
Erik Kluzek <erik@ucar.edu>
2026-08-08 14:05:27 -0600

    Make changes as suggested by Co-pilot in VS-code. Make sure IFS is set correctly at the beginning and restoreed at the end, remove a remaining reference to the old name, better name for the second funciton, quote uses of variables, use local for variables inside functions

--- .github/workflows/check-compset-aliases.sh
+++ .github/workflows/check-compset-aliases.sh
@@ -9,16 +9,20 @@ cd "$(git rev-parse --show-toplevel)"
 # Check that query_config can run without error
 cime/scripts/query_config --compsets 1>/dev/null
 
+# Save previous IFS line-splitting behavior to restore at the end (keep this at the beining)
+OLD_IFS=$IFS
+IFS=$'\n'
+
 bad_compsets() {
   # Subroutine to Find bad compsets
   # Set input arguments
-  ALIAS=$1
-  LCOMPSET=$2
-  OLD_IFS=$IFS
-  IFS='\n'
+  local ALIAS="$1"
+  local LCOMPSET="$2"
   set +e
   # Relies on case sensitivity here: Alias should have $ALIAS and longname should have $LCOMPSET
-  bad_compsets="$(cime/scripts/query_config --compsets | sort | uniq | grep --fixed-strings $ALIAS | grep --fixed-strings --invert-match $LCOMPSET)"
+  # --intervert-match can be shortened to -v
+  # --fixed-strings can be shortened to -F
+  local bad_compsets="$(cime/scripts/query_config --compsets | sort | uniq | grep --fixed-strings -- \"$ALIAS\" | grep --fixed-strings --invert-match -- \"$LCOMPSET\")"
   set -e
   if [[ "${bad_compsets}" != "" ]]; then
       echo "One or more compsets with $ALIAS alias but not $LCOMPSET longname:" >&2
@@ -27,17 +31,17 @@ bad_compsets() {
   fi
 }
 
-negative_bad_compsets() {
-  # Subroutine to Find bad compsets by checking that something is found in the long-compset name if something is NOT in the alias
+check_missing_alias_matches() {
+  # Subroutine to Find bad compsets by checking that if something is not found in the alias -- then something else SHOULD be found in the long-compset name
   # Allows for regular expressions in the alias argument
   # Set input arguments
-  ALIAS=$1
-  LCOMPSET=$2
-  OLD_IFS=$IFS
-  IFS='\n'
+  local ALIAS="$1"
+  local LCOMPSET="$2"
   set +e
   # Relies on case sensitivity here: Alias should have $ALIAS and longname should have $LCOMPSET
-  bad_compsets="$(cime/scripts/query_config --compsets clm | awk 'NR > 5{print $0}' | grep -E --invert-match $ALIAS | grep --fixed-strings --invert-match $LCOMPSET)"
+  # --intervert-match can be shortened to -v
+  # --fixed-strings can be shortened to -F
+  local bad_compsets="$(cime/scripts/query_config --compsets clm | awk 'NR > 5{print $0}' | grep -E --invert-match -- \"$ALIAS\" | grep --fixed-strings -- \"$LCOMPSET\")"
   set -e
   if [[ "${bad_compsets}" != "" ]]; then
       echo "One or more compsets without $ALIAS alias but not $LCOMPSET longname:" >&2
@@ -104,8 +108,11 @@ bad_compsets "Gag " '_CISM2%AIS-EVOLVE%GRIS-EVOLVE_'
 #
 # Now check compsets that don't have something in the alias to make sure something else is there
 #
-negative_bad_compsets "Rs|Rtm|Miz" "_MOSART_"
-negative_bad_compsets "Gs|G |Gag|Ga " "_DGLC%NOEVOLVE_"
-negative_bad_compsets 'Crujra|Cru|Nldas|As|1Pt|Qia|Spi' 'GSWP3v1'
+check_missing_alias_matches "Rs|Rtm|Miz" "_MOSART_"
+check_missing_alias_matches "Gs|G |Gag|Ga " "_DGLC%NOEVOLVE_"
+check_missing_alias_matches 'Crujra|Cru|Nldas|As|1Pt|Qia|Spi' 'GSWP3v1'
+
+# Restore line-splitting behavior (keep this at the end)
+IFS=$OLD_IFS
 
 exit 0
